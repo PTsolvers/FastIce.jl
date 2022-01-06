628a231611e49b439206f243d0cfc1308ef90120
Ludovic Raess <ludovic.rass@gmail.com>
2022-01-06 23:38:41 +0100

    Add preprocessing.

--- benchmarks/SteadyStateGlacier2D.jl
+++ benchmarks/SteadyStateGlacier2D.jl
@@ -1,5 +1,6 @@
 const USE_GPU = haskey(ENV, "USE_GPU") ? parse(Bool, ENV["USE_GPU"]) : false
 const gpu_id  = haskey(ENV, "GPU_ID" ) ? parse(Int , ENV["GPU_ID" ]) : 0
+include(joinpath(@__DIR__, "helpers.jl"))
 ###
 using ParallelStencil
 using ParallelStencil.FiniteDifferences2D
@@ -9,7 +10,7 @@ using ParallelStencil.FiniteDifferences2D
 else
     @init_parallel_stencil(Threads, Float64, 2)
 end
-using Plots, Printf, Statistics, LinearAlgebra, MAT, Random
+using Plots, Printf, Statistics, LinearAlgebra, MAT, Random, UnPack
 
 import ParallelStencil: INDICES
 ix,iy = INDICES[1], INDICES[2]
@@ -47,31 +48,28 @@ end
     return
 end
 
-function is_inside_fluid(x,y,gl)
-    return (x+0.1*gl)*(x+0.1*gl) + y*y < gl*gl
+function is_inside_fluid(y,ysurf)
+    return y < ysurf
 end
 
-function is_inside_solid(x,y,lx,amp,ω,tanβ,el)
-    return y < amp*sin(ω*x/lx) + tanβ*x + el
+function is_inside_solid(y,ybed)
+    return y < ybed
 end
 
-@parallel_indices (ix,iy) function init_ϕ!(ϕ,gl,el,tanβ,ω,amp,dx,dy,lx,ly)
-    xc,yc = dx*ix-dx/2-lx/2, dy*iy-dy/2
+@parallel_indices (ix,iy) function init_ϕ!(ϕ,ybed,ysurf,yc)
     if checkbounds(Bool,ϕ,ix,iy)
-        if is_inside_fluid(xc,yc,gl)
+        if is_inside_fluid(yc[iy],ysurf[ix])
             ϕ[ix,iy] = fluid
         end
-        if is_inside_solid(xc,yc,lx,amp,ω,tanβ,el)
+        if is_inside_solid(yc[iy],ybed[ix])
             ϕ[ix,iy] = solid
         end
     end
     return
 end
 
-@views function Stokes2D(xc, ybed, ysurf)
-    
-    ly  = max(ybed) - min(ybed)
-    
+@views function Stokes2D(inputs::InputParams)
+    @unpack ybed, ysurf, xc, yc, xv, yv, lx_ly, nx, ny, α = inputs
     # physics
     ## dimensionally independent
     ly        = 1.0               # domain height    [m]
@@ -81,29 +79,14 @@ end
     psc       = ρg0*ly
     tsc       = μs0/psc
     vsc       = ly/tsc
-    ## nondimensional parameters
-    lx_ly     = 2.0
-    gl_ly     = 0.9
-    el_ly     = 0.3
-    amp_ly    = 1/25
-    α         = 0*π/12
-    tanβ      = tan(-π/12)
-    ωly       = 10π
     ## dimensionally dependent
     lx        = lx_ly*ly
-    gl        = gl_ly*ly
-    el        = el_ly*ly
-    amp       = amp_ly*ly
     ρgx       = ρg0*sin(α)
     ρgy       = ρg0*cos(α)
-    ω         = ωly/ly
     # numerics
-    ny        = 256
-    nx        = ceil(Int,lx_ly*ny)
-    nx, ny    = nx-1, ny-1
-    maxiter   = 50ny         # maximum number of pseudo-transient iterations
-    nchk      = 2*ny         # error checking frequency
-    nviz      = 2*ny         # visualisation frequency
+    maxiter   = 10nx         # maximum number of pseudo-transient iterations
+    nchk      = nx           # error checking frequency
+    nviz      = nx           # visualisation frequency
     ε_V       = 1e-8         # nonlinear absolute tolerence for momentum
     ε_∇V      = 1e-8         # nonlinear absolute tolerence for divergence
     CFL       = 0.95/sqrt(2) # stability condition
@@ -111,13 +94,11 @@ end
     r         = 1.0          # Bulk to shear elastic modulus ratio (numerical parameter #2)
     # preprocessing
     dx, dy    = lx/nx, ly/ny # cell sizes
-    max_lxy   = 0.5gl
+    max_lxy   = 0.04*lx
     Vpdτ      = min(dx,dy)*CFL
     dτ_ρ      = Vpdτ*max_lxy/Re/μs0
     Gdτ       = Vpdτ^2/dτ_ρ/(r+2.0)
     μ_veτ     = 1.0/(1.0/Gdτ + 1.0/μs0)
-    Xc, Yc    = LinRange(-(lx-dx)/2,(lx-dx)/2,nx  ), LinRange(dy/2,ly-dy/2,ny  )
-    Xv, Yv    = LinRange(- lx/2    , lx/2    ,nx+1), LinRange(0   ,ly     ,ny+1)
     # allocation
     Pt        = @zeros(nx  ,ny  )
     ∇V        = @zeros(nx  ,ny  )
@@ -129,10 +110,12 @@ end
     Vx        = @zeros(nx+1,ny  )
     Vy        = @zeros(nx  ,ny+1)
     ϕ         = fill(air,nx,ny)
-    Vx_v      = copy(Vx) # visu
-    Vy_v      = copy(Vy) # visu
+    Vx_v      = copy(Vx[2:end,:]) # visu
+    Vy_v      = copy(Vy[:,2:end]) # visu
     Pt_v      = copy(Pt) # visu
-    @parallel init_ϕ!(ϕ,gl,el,tanβ,ω,amp,dx,dy,lx,ly)
+    ybed      = Data.Array(ybed)
+    ysurf     = Data.Array(ysurf)
+    @parallel init_ϕ!(ϕ,ybed,ysurf,yc)
     # iteration loop
     err_V=2*ε_V; err_∇V=2*ε_∇V; iter=0; err_evo1=[]; err_evo2=[]
     while !((err_V <= ε_V) && (err_∇V <= ε_∇V)) && (iter <= maxiter)
@@ -146,31 +129,49 @@ end
             norm_∇V = norm((ϕ.==fluid).*∇V)/vsc*lx/sqrt(length(∇V))
             err_V   = maximum([norm_Rx, norm_Ry])
             err_∇V  = norm_∇V
-            push!(err_evo1, maximum([norm_Rx, norm_Ry, norm_∇V])); push!(err_evo2,iter/ny)
+            push!(err_evo1, maximum([norm_Rx, norm_Ry, norm_∇V])); push!(err_evo2,iter/nx)
             @printf("# iters = %d, err_V = %1.3e [norm_Rx=%1.3e, norm_Ry=%1.3e], err_∇V = %1.3e \n", iter, err_V, norm_Rx, norm_Ry, err_∇V)
         end
         if iter % nviz == 0
-            Vx_v .= Vx; Vx_v[Vx.==0] .= NaN
-            Vy_v .= Vy; Vy_v[Vy.==0] .= NaN
+            Vxa = 0.5*(Vx[1:end-1,:].+Vx[2:end,:])
+            Vya = 0.5*(Vy[:,1:end-1].+Vy[:,2:end])
+            Vxr = Vxa.*cos(α) .- Vya.*sin(α)
+            Vyr = Vxa.*sin(α) .+ Vya.*cos(α)
+            Vx_v .= Vxr; Vx_v[Vxr.==0] .= NaN
+            Vy_v .= Vyr; Vy_v[Vyr.==0] .= NaN
             Pt_v .= Pt; Pt_v[Pt.==0] .= NaN
             fntsz = 7
-            opts  = (aspect_ratio=1, xlims=(Xv[1],Xv[end]), ylims=(Yc[1],Yc[end]), yaxis=font(fntsz,"Courier"), xaxis=font(fntsz,"Courier"), framestyle=:box, titlefontsize=fntsz, titlefont="Courier")
+            opts  = (aspect_ratio=4, xlims=(xv[1],xv[end]), ylims=(yc[1],yc[end]), yaxis=font(fntsz,"Courier"), xaxis=font(fntsz,"Courier"), framestyle=:box, titlefontsize=fntsz, titlefont="Courier")
             opts2 = (linewidth=2, markershape=:circle, markersize=3,yaxis = (:log10, font(fntsz,"Courier")), xaxis=font(fntsz,"Courier"), framestyle=:box, titlefontsize=fntsz, titlefont="Courier")
-            p1 = heatmap(Xv,Yc,Array(Vx_v)'; c=:batlow, title="Vx", opts...)
-            p2 = heatmap(Xc,Yv,Array(Vy_v)'; c=:batlow, title="Vy", opts...)
-            p3 = heatmap(Xc,Yc,Array(Pt_v)'; c=:viridis, title="Pressure", clims=(0.0, 0.6), opts...)
+            p1 = heatmap(xc,yc,Array(Vx_v)'; c=:batlow, title="Vx", opts...)
+            p2 = heatmap(xc,yc,Array(Vy_v)'; c=:batlow, title="Vy", opts...)
+            p3 = heatmap(xc,yc,Array(Pt_v)'; c=:viridis, title="Pressure", #=clims=(0.0, 0.6),=# opts...)
             p4 = plot(err_evo2,err_evo1; legend=false, xlabel="# iterations/nx", ylabel="log10(error)", labels="max(error)", opts2...)
             display(plot(p1, p2, p3, p4, size=(1e3,600), dpi=200))
         end
     end
     return
 end
+
 # ---------------------
-include(joinpath(@__DIR__, "helpers.jl"))
 
 # load the data
-println("Loading the data ... ")
-xc, ybed, ysurf = read_data("../data/arolla51.txt"; resol=512, visu=true)
-println("done.")
+xv, ybed, ysurf = read_data("../data/arolla51.txt"; resol=1024)
 
-# Stokes2D(xc, ybed, ysurf)
+# preprocessing
+inputs, lin_fit, y_avg, orig  = preprocess(xv, ybed, ysurf; do_rotate=true, fact_ny=2)
+
+visu = true
+if visu
+    p1 = plot(xv, ybed   , label="bedrock", linewidth=3)
+        plot!(xv, ysurf  , label="surface", linewidth=3)
+        plot!(xv, y_avg  , label="y_avg"  , linewidth=3)
+        plot!(xv, lin_fit, label="lin_fit", linewidth=3)
+    p2 = plot(xv, inputs.ybed , label="bedrock", linewidth=3)
+        plot!(xv, inputs.ysurf, label="bedrock", linewidth=3, legend=false)
+    display(plot(p1,p2, layout = (2,1)))
+end
+
+
+Stokes2D(inputs)
+
--- benchmarks/helpers.jl
+++ benchmarks/helpers.jl
@@ -1,33 +1,139 @@
 using DelimitedFiles, Plots, Interpolations
 
+"Heuristic number of threads per block in x-dim."
+const tx = 32
+
+"Heuristic number of threads per block in y-dim."
+const ty = 8
+
+"Input parameters structure"
+mutable struct InputParams
+    ybed
+    ysurf
+    xc
+    yc
+    xv
+    yv
+    lx_ly
+    nx::Int
+    ny::Int
+    α
+end
+
+
 """
-    read_data(dat_file::String; resol::Int=128, visu=false)
+    read_data(dat_file::String; resol::Int=128, olen::Int=1, visu=false)
 
-Reads bedrock and ice elevation data for given x-coords from a text file and returns them interpolated on a finer grid.
+Read bedrock and ice surface elevation data from a text file and returns the interpolated data.
 
-The `dat_file` is expected to have 3 space-limited columns as `[x-coord  z-bedrock  z-surface]`
+# Arguments
+- `dat_file::String`: input data file with 3 space-limited columns as `[x-coord  z-bedrock  z-surface]`
+- `resol::Int=128`: output resolution
+- `olen::Int=1`: overlength for arrays larger then `nx` and `ny`
 """
-function read_data(dat_file::String; resol::Int=128, visu=false)
-
+function read_data(dat_file::String; resol::Int=128, olen::Int=1)
+    
+    println("Loading the data ... ")
     data = readdlm(dat_file, Float64)
 
-    xc_d   = data[:,1]
+    xv_d   = data[:,1]
     bed_d  = data[:,2]
     surf_d = data[:,3]
 
-    xc    = LinRange(xc_d[1], xc_d[end], resol)
-    itp1  = interpolate( (xc_d,), bed_d[:,1],  Gridded(Linear()))
-    itp2  = interpolate( (xc_d,), surf_d[:,1], Gridded(Linear()))
-    bed   = itp1.(xc)
-    surf  = itp2.(xc)
-    
-    @assert length(xc) == size(bed)[1] == size(surf)[1]
-    println("Interpolating original data (nx=$(size(bed_d)[1])) on nx=$(size(bed)[1]) grid.")
+    resol  = resol > tx ? resol : tx
+    shiftx = resol % tx
+    resx   = (shiftx < tx/2 ? Int(resol - shiftx) : Int(resol + tx - shiftx)) - olen
 
-    if visu
-        plot(xc, bed, label="bedrock", linewidth=3)
-        display(plot!(xc, surf, label="surface", linewidth=3))
+    xv     = LinRange(xv_d[1], xv_d[end], resol)
+    itp1   = interpolate( (xv_d,), bed_d[:,1] , Gridded(Linear()))
+    itp2   = interpolate( (xv_d,), surf_d[:,1], Gridded(Linear()))
+    bed    = itp1.(xv)
+    surf   = itp2.(xv)
+    
+    @assert length(xv) == size(bed)[1] == size(surf)[1]
+    println("Interpolating original data (nxv=$(size(bed_d)[1])) on nxv=$(size(bed)[1]) grid.")
+    println("done.")
+    return xv, bed, surf
+end
+
+
+"""
+    rotate(ybed, ysurf, lin_fit)
+
+Rotate bedrock `ybed` and surface `ysurf` profiles. 
+"""
+function rotate(ybed, ysurf, lin_fit)
+    ybedr  = ybed  .- lin_fit
+    ysurfr = ysurf .- lin_fit
+    ymin   = minimum(ybedr)
+    ybedr  = ybedr  .- ymin
+    ysurfr = ysurfr .- ymin
+    return ybedr, ysurfr
+end
+
+
+"""
+    ls_fit(xv, ybed, ysurf)
+
+Perform linear least-square fit of mean bedrock and surface data.
+"""
+function ls_fit(xv, ybed, ysurf)
+    nxv     = length(xv)
+    x_mean  = sum(xv) ./ nxv
+    y_avg   = (ybed .+ ysurf) ./ 2.0
+    y_mean  = sum(y_avg) ./ nxv
+    α       = sum((xv .- x_mean) .* (y_avg .- y_mean)) ./ sum((xv .- x_mean).^2)
+    orig    = y_mean .- α .* x_mean
+    lin_fit = α.*xv .+ orig
+    return lin_fit, y_avg, orig, α
+end
+
+
+"""
+    preprocess(xv, ybed, ysurf; do_rotate=false, fact_ny::Int=4, olen::Int=1, visu=false)
+
+Preprocess input data for iceflow model.
+
+# Arguments
+- `xv`: x-dim vertice coordinates
+- `ybed`: bedrock elevation
+- `ysurf`: surface elevation
+- `do_rotate=false`: rotate domain to minimise non-used grid-points
+- `fact_ny::Int=4`: grid-point increase in y-dim
+- `olen::Int=1`: overlength for arrays larger then `nx` and `ny`
+"""
+function preprocess(xv, ybed, ysurf; do_rotate=false, fact_ny::Int=4, olen::Int=1)
+    
+    println("Starting preprocessing ... ")
+    lin_fit, y_avg, orig, α = ls_fit(xv, ybed, ysurf)
+
+    if do_rotate
+        ybedr, ysurfr = rotate(ybed, ysurf, lin_fit)
+    else
+        ybedr, ysurfr = ybed, ysurf
     end
-    return xc, bed, surf
+
+    ymin   = minimum(ybedr)
+    ybedr  = ybedr  .- ymin
+    ysurfr = ysurfr .- ymin
+    ly     = maximum(ysurfr)
+    lx     = maximum(xv) - minimum(xv)
+    xc     = 0.5(xv[1:end-1].+xv[2:end])
+    nx     = length(xc)
+    resy   = ceil(Int, ly/lx*nx)
+    resy   = resy > tx ? resy : tx
+    shifty = resy % ty
+    ny     = fact_ny * (shifty < ty/2 ? Int(resy - shifty) : Int(resy + ty - shifty)) - olen
+    nyv    = ny+1
+    yv     = LinRange(0,ly,nyv)
+    yc     = 0.5(yv[1:end-1].+yv[2:end])
+
+    println("Preprocessed data: nx=$nx, ny=$ny (dx=$(lx/nx), dx=$(ly/ny))")
+
+    inputs = InputParams(ybedr/ly, ysurfr/ly, xc/ly, yc/ly, xv/ly, yv/ly, lx/ly, nx, ny, α) # nondim
+    # inputs = InputParams(ybedr, ysurfr, xc, yc, xv, yv, lx, nx, ny, α) # dim
+    
+    println("done.")
+    return inputs, lin_fit, y_avg, orig
 end
 
