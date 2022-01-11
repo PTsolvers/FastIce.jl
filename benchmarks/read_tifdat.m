ec6793d3ed47b2c84bab2298356e026e0fbf0157
Ludovic Raess <ludovic.rass@gmail.com>
2022-01-11 16:33:07 +0100

    Add 3D helpers and Arolla glacier data.

--- benchmarks/read_tifdat.m
+++ benchmarks/read_tifdat.m
@@ -7,33 +7,98 @@ path_data = '../data';
 name_bed  = 'B73-12_GlacierBed';
 name_surf = 'B73-12_SwissALTI3D_r2019';
 
-tif      = Tiff([path_data, '/', name_bed, '.tif'],'r');
-img_bed  = read(tif);
-tif      = Tiff([path_data, '/', name_surf, '.tif'],'r');
-img_surf = read(tif);
-
-bed  = img_bed;  bed2  = bed; 
-surf = img_surf; surf2 = surf;
-
-
-bed2(bed<(no_dat+1)) = NaN;
-
-surf2(bed<(no_dat+1)) = no_dat;
-surf2(surf2<(no_dat+1)) = NaN;
-
-ele = surf2 - bed2;
+tif   = Tiff([path_data, '/', name_bed, '.tif'],'r');
+zbed  = read(tif);
+tif   = Tiff([path_data, '/', name_surf, '.tif'],'r');
+zsurf = read(tif);
 
 % crop
 ystart = 168;
 xend   = 319;
-bed_dat  = double(bed2(1:xend,ystart:end));
-surf_dat = double(surf2(1:xend,ystart:end));
-ele_dat  = double(ele(1:xend,ystart:end));
+zbed   = double(zbed(1:xend,ystart:end));
+zsurf  = double(zsurf(1:xend,ystart:end));
 
+% preprocess
+mask   = abs(1-(zbed==no_dat));
+zthick = zsurf - zbed;
+zavg   = 0.5.*(zsurf + zbed);
+
+dx  = 10;
+dy  = 10;
+nxv = size(zbed,1);
+nyv = size(zbed,2);
+Lx  = dx*(nxv-1);
+Ly  = dy*(nyv-1);
+xv  = 0:dx:Lx;
+yv  = 0:dy:Ly;
+[xv2,yv2] = ndgrid(xv,yv);
+
+if do_save==1, save([path_data, '/', 'arolla3D.mat'], 'zbed', 'zsurf', 'mask', 'xv', 'yv'); end
+
+xv2   = mask.*xv2;
+yv2   = mask.*yv2;
+zavg  = mask.*zavg;
+
+% LSQ fit
+xv2_  = xv2;  xv2_(mask==0)=[];
+yv2_  = yv2;  yv2_(mask==0)=[];
+zavg_ = zavg; zavg_(mask==0)=[];
+
+A = [xv2_(:), yv2_(:), ones(size(xv2_(:)))];
+B = zavg_(:);
+
+x = (A'*A)\(A'*B);
+
+plane = xv2*x(1) + yv2*x(2) + x(3);
+
+% rotate
+zbedr  = (zbed  - plane).*mask;
+zsurfr = (zsurf - plane).*mask;
+zmin   = min(zbedr(:));
+zbedr  = zbedr  - zmin;
+zsurfr = zsurfr - zmin;
+
+% preprocess
+zmin   = min(zbedr(:));
+zbedr  = zbedr  - zmin;
+zsurfr = zsurfr - zmin;
+maxDz  = max((zsurf(:) - zbed(:)).*mask(:)); % redundant
+lz     = max(zsurfr(:));
+lx     = max(xv) - min(xv);
+ly     = max(yv) - min(yv);
+xc     = 0.5*(xv(1:end-1)+xv(2:end));
+yc     = 0.5*(yv(1:end-1)+yv(2:end));
+nx     = length(xc);
+ny     = length(yc);
+resz   = ceil(lz/lx*nx);
+% resz   = resz > tx ? resz : tx
+% shiftz = resz % tz
+nz     = resz; % fact_ny * (shifty < ty/2 ? Int(resy - shifty) : Int(resy + ty - shifty)) - olen
+nzv    = nz+1;
+zv     = linspace(0,lz,nzv);
+zc     = 0.5*(zv(1:end-1)+zv(2:end));
+
+% visu
+xv2_v = xv2; xv2_v(mask==0)=NaN;
+yv2_v = yv2; yv2_v(mask==0)=NaN;
+zbed_v   = zbed;   zbed_v(mask==0)=NaN;
+zsurf_v  = zsurf;  zsurf_v(mask==0)=NaN;
+zthick_v = zthick; zthick_v(mask==0)=NaN;
+zavg_v   = zavg;   zavg_v(mask==0)=NaN;
+plane_v  = plane;  plane_v(mask==0)=NaN;
+zbedr_v  = zbedr;  zbedr_v(mask==0)=NaN;
+zsurfr_v = zsurfr; zsurfr_v(mask==0)=NaN;
 
 figure(1),clf
-subplot(311), pcolor(bed_dat'), shading flat, axis equal tight, colorbar, title('bedrock elevation')
-subplot(312), pcolor(surf_dat'), shading flat, axis equal tight, colorbar, title('surface elevation')
-subplot(313), pcolor(ele_dat'), shading flat, axis equal tight, colorbar, title('ice thickness')
+subplot(211)
+scatter3(xv2_v(:), yv2_v(:), zavg_v(:), 6, zavg_v(:), 'filled')
+hold on, scatter3(xv2_v(:), yv2_v(:), plane(:),'filled'), hold off
+subplot(212)
+scatter3(xv2_v(:), yv2_v(:), zsurfr_v(:), 6, zsurfr_v(:), 'filled')
+hold on, scatter3(xv2_v(:), yv2_v(:), zbedr_v(:), 6, zbedr_v(:), 'filled'), hold off
 
-if do_save==1, save([path_data, '/', 'arolla3D.mat'], 'bed_dat','surf_dat','ele_dat'); end
+
+figure(2),clf
+subplot(311), pcolor(zbed_v'), shading flat, axis equal tight, colorbar, title('bedrock elevation')
+subplot(312), pcolor(zsurf_v'), shading flat, axis equal tight, colorbar, title('surface elevation')
+subplot(313), pcolor(zthick_v'), shading flat, axis equal tight, colorbar, title('ice thickness')
