58ba6c487a9544d4f1e677dfc61e5e136f7f676a
Ludovic Raess <ludovic.rass@gmail.com>
2022-02-03 21:26:00 +0100

    Getting closer. WIP.

--- benchmarks/helpers3D_v3.jl
+++ benchmarks/helpers3D_v3.jl
@@ -1,4 +1,4 @@
-using Statistics, GeoArrays, Interpolations, LinearAlgebra#, Plots
+using Statistics, GeoArrays, Interpolations, LinearAlgebra, MAT, PyPlot
 
 const USE_GPU = haskey(ENV, "USE_GPU") ? parse(Bool, ENV["USE_GPU"]) : false
 const gpu_id  = haskey(ENV, "GPU_ID" ) ? parse(Int , ENV["GPU_ID" ]) : 0
@@ -66,6 +66,7 @@ function my_filter(A, mask)
     return [A[i] for i in eachindex(A) if mask[i] != 0]
 end
 
+
 """
     lsq_fit(mask, zavg, xv2, yv2)
 
@@ -87,6 +88,25 @@ Linear least-square fit of mean bedrock and surface data.
     # best fitting plane
     plane   = xv2*x[1] .+ yv2*x[2] .+ x[3];
     return plane, x
+end
+
+
+"Rotate field `A`, `x2v`, `y2v` with rotation matrix `R`."
+function my_rot(R, X, Y, Z)
+    xrot = R[1,1].*X .+ R[1,2].*Y .+ R[1,3].*Z
+    yrot = R[2,1].*X .+ R[2,2].*Y .+ R[2,3].*Z
+    zrot = R[3,1].*X .+ R[3,2].*Y .+ R[3,3].*Z
+    return xrot, yrot, zrot
+end
+
+
+"Rotate field `A`, `x2v`, `y2v` with rotation matrix `R` and return extent."
+function my_rot_minmax(R, X, Y, Z)
+    xrot, yrot, zrot = my_rot(R, X, Y, Z)
+    xmin,xmax = minimum(xrot), maximum(xrot)
+    ymin,ymax = minimum(yrot), maximum(yrot)
+    zmin,zmax = minimum(zrot), maximum(zrot)
+    return xmin, xmax, ymin, ymax, zmin, zmax
 end
 
 
@@ -123,17 +143,6 @@ end
 end
 
 
-# "Round phases after applying smoothing."
-# @parallel_indices (ix,iy,iz) function round_phase!(A)
-#     if checkbounds(Bool,A,ix,iy,iz)
-#         if (A[ix,iy,iz] <= 0.9                      ) A[ix,iy,iz] = air   end
-#         if (A[ix,iy,iz] >  0.9 && A[ix,iy,iz] <= 1.9) A[ix,iy,iz] = fluid end
-#         if (A[ix,iy,iz] >  1.9                      ) A[ix,iy,iz] = solid end
-#     end
-#     return
-# end
-
-
 """
     preprocess(dat_file::String; resx::Int=128, resy::Int=128, do_rotate::Bool=true, do_nondim::Bool=true, fact_nz::Int=2, olen::Int=1)
 
@@ -151,50 +160,52 @@ Preprocess input data for iceflow model.
 @views function preprocess(dat_name::String; resx::Int=128, resy::Int=128, do_rotate::Bool=true, do_nondim::Bool=true, fact_nz::Int=2, olen::Int=1)
     
     println("Starting preprocessing ... ")
-    nodat_bed = -10
 
-    # load the data
     println("- load the data")
     file1  = ("../data/alps/IceThick_cr0_$(dat_name).tif")
     file2  = ("../data/alps/BedElev_cr_$(dat_name).tif"  )
     zthick = reverse(GeoArrays.read(file1)[:,:,1], dims=2)
     zbed   = reverse(GeoArrays.read(file2)[:,:,1], dims=2)
-    coord  = reverse(GeoArrays.coords(GeoArrays.read(file2)), dims=2)
+    coord  = reverse(GeoArrays.coords(GeoArrays.read(file2)), dims=2)  
+
+    # DEBUG: a step here could be rotation of the (x,y) plane using bounding box (rotating calipers)
+    
+    # coords and centre data in x,y plane
+    (x2v,y2v)  = (getindex.(coord,1), getindex.(coord,2))
+    xmin, xmax = extrema(x2v)
+    ymin, ymax = extrema(y2v)
+    ∆x, ∆y     = xmax-xmin, ymax-ymin
+    x2v       .= x2v .- xmin .- ∆x/2
+    y2v       .= y2v .- ymin .- ∆y/2
 
     # define and apply masks
     mask = ones(size(zthick))
     mask[ismissing.(zthick)] .= 0
+
     zthick[mask.==0] .= 0
+    zthick = convert(Matrix{Float64}, zthick)
 
-    zbed[ismissing.(zbed)] .= nodat_bed
-    zbed[zbed.==nodat_bed] .= mean(my_filter(zbed,mask))
+    zbed[ismissing.(zbed)] .= mean(my_filter(zbed,mask))
+    zbed = convert(Matrix{Float64}, zbed)
 
-    # define surface and avg
+    # define surface and avg topo
     zsurf = zbed .+ zthick
     zavg  = 0.5(zsurf .+ zbed)
 
-    # retrieve extrema
-    # zmin, zmax = minimum(zbed),maximum(zsurf)
+    # retrieve extrema and centre data
     zmin, zmax = minimum( my_filter(zbed,mask) ),maximum( my_filter(zsurf,mask) )
-    (x2v,y2v)  = (getindex.(coord,1), getindex.(coord,2))
-    xmin, xmax = extrema(x2v)
-    ymin, ymax = extrema(y2v)
+    ∆z         = zmax-zmin
+    zbed      .= zbed  .- zmin
+    zsurf     .= zsurf .- zmin
+    zavg      .= zavg  .- zmin
 
     if do_rotate
         println("- perform least square fit")
         plane, x = lsq_fit(mask, zavg, x2v, y2v)
         αx, αy, ori = x[1], x[2], x[3]
-        ox,oy,oz = -(xmax-xmin)/2,-(ymax-ymin)/2,-ori + zmin
     else
         αx, αy, ori = 0.0, 0.0, 0.0
-        ox,oy,oz = -(xmax-xmin)/2,-(ymax-ymin)/2,-(zmax-zmin)/2
     end
-
-    # center data
-    zbed        = zbed  .- zmin .+ oz
-    zsurf       = zsurf .- zmin .+ oz
-    x2v         = x2v   .- xmin .+ ox
-    y2v         = y2v   .- ymin .+ oy
  
     nv          = [-αx -αy 1.0]
     nv        ./= norm(nv)
@@ -210,26 +221,18 @@ Preprocess input data for iceflow model.
          ax[2]*ax[1]*(1-cos(θ))     cos(θ) + ax[2]^2*(1-cos(θ)) -ax[1]*sin(θ)
         -ax[2]*sin(θ)               ax[1]*sin(θ)                       cos(θ)]
 
+    # DEBUG: one could here stop and export an HDF5 file including data, x,y coords, rotation matrix and ori
+    # ----------------
+    # DEBUG: since here, it could be done in code
     # rotate surface
-    xsrot       =   R[1,1]*x2v .+ R[1,2]*y2v .+ R[1,3]*zsurf
-    ysrot       =   R[2,1]*x2v .+ R[2,2]*y2v .+ R[2,3]*zsurf
-    zsrot       =   R[3,1]*x2v .+ R[3,2]*y2v .+ R[3,3]*zsurf
-    xsmin,xsmax = minimum(xsrot), maximum(xsrot)
-    ysmin,ysmax = minimum(ysrot), maximum(ysrot)
-    zsmin,zsmax = minimum(zsrot), maximum(zsrot)
+    xsmin, xsmax, ysmin, ysmax, zsmin, zsmax = my_rot_minmax(R, x2v, y2v, zsurf)
 
     # rotate bed
-    xbrot       =   R[1,1]*x2v .+ R[1,2]*y2v .+ R[1,3]*zbed
-    ybrot       =   R[2,1]*x2v .+ R[2,2]*y2v .+ R[2,3]*zbed
-    zbrot       =   R[3,1]*x2v .+ R[3,2]*y2v .+ R[3,3]*zbed
-    xbmin,xbmax = minimum(xbrot), maximum(xbrot)
-    ybmin,ybmax = minimum(ybrot), maximum(ybrot)
-    zbmin,zbmax = minimum(zbrot), maximum(zbrot)
+    xbmin, xbmax, ybmin, ybmax, zbmin, zbmax = my_rot_minmax(R, x2v, y2v, zbed)
 
     # get extents
-    xrmin,xrmax = min(xsmin,xbmin),max(xsmax,xbmax)
-    yrmin,yrmax = min(ysmin,ybmin),max(ysmax,ybmax)
-    # zrmin,zrmax = min(zsmin,zbmin),max(zsmax,zbmax)
+    xrmin,xrmax = min(xsmin,xbmin), max(xsmax,xbmax)
+    yrmin,yrmax = min(ysmin,ybmin), max(ysmax,ybmax)
     zrmin,zrmax = zbmin, zbmax
     lx,ly,lz    = xrmax-xrmin, yrmax-yrmin, zrmax-zrmin
 
@@ -246,8 +249,8 @@ Preprocess input data for iceflow model.
     zbed2, zthick2 = interp(zbed, zthick, xv_d, yv_d, xv, yv)
     println("- interpolate original data (nxv, nyv = $(size(zbed)[1]), $(size(zbed)[2])) on nxv, nyv = $(size(zbed2)[1]), $(size(zbed2)[2]) grid")
 
-    nsmb = 5#ceil(Int,nx/20)
-    nsmt = 10#ceil(Int,nx/20)
+    nsmb = 0#ceil(Int,nx/20)
+    nsmt = 0#ceil(Int,nx/20)
     println("- apply smoothing ($nsmb steps on bed, $nsmt steps on thickness)")
     for ismb=1:nsmb
         smooth2D!(zbed2, 1.0)
@@ -259,16 +262,17 @@ Preprocess input data for iceflow model.
     # reconstruct surface
     zsurf2 = zbed2 .+ zthick2
 
-    # 3D grid
-    xc, yc, zc  = LinRange(xrmin-0.01lx,xrmax+0.01lx,nx), LinRange(yrmin-0.01ly,yrmax+0.01ly,ny), LinRange(zrmin-0.01lz,zrmax+0.01lz,nz)
-    dx, dy, dz  = xc[2]-xc[1], yc[2]-yc[1], zc[2]-zc[1]
-    (x3,y3,z3)  = ([x for x=xc,y=yc,z=zc], [y for x=xc,y=yc,z=zc], [z for x=xc,y=yc,z=zc])
+    # display(heatmap(xv, yv, zthick2'))
+    # error("stop")
 
-    Rinv    = R'
+    # 3D grid
+    xc, yc, zc = LinRange(xrmin-0.01lx,xrmax+0.01lx,nx), LinRange(yrmin-0.01ly,yrmax+0.01ly,ny), LinRange(zrmin-0.01lz,zrmax+0.01lz,nz)
+    dx, dy, dz = xc[2]-xc[1], yc[2]-yc[1], zc[2]-zc[1]
+    (x3,y3,z3) = ([x for x=xc,y=yc,z=zc], [y for x=xc,y=yc,z=zc], [z for x=xc,y=yc,z=zc])
+
     # rotate grid
-    x3rot   =  Rinv[1,1]*x3 .+ Rinv[1,2]*y3 .+ Rinv[1,3]*z3
-    y3rot   =  Rinv[2,1]*x3 .+ Rinv[2,2]*y3 .+ Rinv[2,3]*z3
-    z3rot   =  Rinv[3,1]*x3 .+ Rinv[3,2]*y3 .+ Rinv[3,3]*z3
+    Rinv   = R'
+    x3rot, y3rot, z3rot = my_rot(Rinv, x3, y3, z3)
     
     # set phases
     println("- set phases (0-air, 1-ice, 2-bedrock)")
@@ -278,25 +282,35 @@ Preprocess input data for iceflow model.
     z3rot  = Data.Array(z3rot)
     zsurf2 = Data.Array(zsurf2)
     zbed2  = Data.Array(zbed2)
-    @parallel set_phases!(ϕ, x3rot, y3rot, z3rot, zsurf2, zbed2, ox, oy, dx, dy)
+    @parallel set_phases!(ϕ, x3rot, y3rot, z3rot, zsurf2, zbed2, -xrmax, -yrmax, dx, dy)
     
     sc     = do_nondim ? 1.0/lz : 1.0    
     inputs = InputParams3D(ϕ, x3rot*sc, y3rot*sc, z3rot*sc, x3*sc, y3*sc, z3*sc, xc*sc, yc*sc, zc*sc, R, lx*sc, ly*sc, lz*sc, nx, ny, nz, sc)
 
     # vv = copy(zbrot);  vv[mask.==0] .=NaN
-    # sl = copy(ϕ)
-    # clf()
-    # subplot(131),pcolor(vv'),colorbar()
-    # # subplot(132),pcolor(sl[:,ceil(Int,ny/4),:]'),colorbar()
-    # subplot(132),pcolor(x3rot[:,ceil(Int,ny/4),:], z3rot[:,ceil(Int,ny/4),:], sl[:,ceil(Int,ny/4),:]),colorbar()
-    # # subplot(133),pcolor(sl[200,:,:]'),colorbar()
-    # subplot(133),pcolor(y3rot[ceil(Int,3*nx/4),:,:], z3rot[ceil(Int,3*nx/4),:,:], sl[ceil(Int,3*nx/4),:,:]),colorbar()
+    sl = copy(ϕ)
+    # p1 = heatmap(sl[:,1,:]')
+    # p2 = heatmap(sl[65,:,:]')
+    # p3 = heatmap(sl[:,85,:]')
+    # p4 = heatmap(sl[:,115,:]')
+    # p2 = heatmap(sl[Int(ceil(size(sl,1)/3)),:,:]')
+    # display(plot(p1,p2, p3, p4))
+    clf()
+    subplot(311),pcolor(x3rot[:,ceil(Int,  ny/4),:], z3rot[:,ceil(Int,  ny/4),:], sl[:,ceil(Int,  ny/4),:]),colorbar()
+    subplot(312),pcolor(x3rot[:,ceil(Int,  ny/2),:], z3rot[:,ceil(Int,  ny/2),:], sl[:,ceil(Int,  ny/2),:]),colorbar()
+    subplot(313),pcolor(x3rot[:,ceil(Int,3*ny/4),:], z3rot[:,ceil(Int,3*ny/4),:], sl[:,ceil(Int,3*ny/4),:]),colorbar()
     
-    # display(Plot.heatmap(xv,yv,zthick2'))
+    # !ispath("../out_visu") && mkdir("../out_visu")
+    # matwrite("../out_visu/out_pa3D.mat",
+    #     Dict("Phase"=> Array(ϕ),
+    #          "x3rot"=> Array(x3rot*sc), "y3rot"=> Array(y3rot*sc), "z3rot"=> Array(z3rot*sc),
+    #          "x3"=> Array(x3*sc), "y3"=> Array(y3*sc), "z3"=> Array(z3*sc),
+    #          "xc"=> Array(xc*sc), "yc"=> Array(yc*sc), "zc"=> Array(zc*sc),
+    #          "lx"=> lx, "ly"=> ly, "lz"=> lz, "sc"=> sc); compress = true)
 
     println("... done.")
     return inputs
 end
 
 # preprocessing
-inputs = preprocess("Rhone"; resx=256, resy=256, do_rotate=true, fact_nz=2)
+inputs = preprocess("Rhone"; resx=256, resy=256, do_rotate=true, fact_nz=1)
