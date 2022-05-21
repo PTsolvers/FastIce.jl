a1f9841f0e2724f02bd535de499a242fe30dd7b5
Ludovic Raess <ludovic.rass@gmail.com>
2022-05-21 16:04:27 +0200

    Add a fix for accurately computing shear stress in cells with > 3 faces being fluid.

--- scripts3D/flagging_macros3D.jl
+++ scripts3D/flagging_macros3D.jl
@@ -113,3 +113,30 @@ macro not_in_phases_zi(ϕ, p1, p2, expr)
     bnd_chk = :( ix <= size($ϕ,1)-2 && iy <= size($ϕ,2)-2 && iz <= size($ϕ,3)-1 )
     esc(:( if $bnd_chk && (($ϕ[ix+1,iy+1,iz] != $p1 && $ϕ[ix+1,iy+1,iz+1] != $p2) || ($ϕ[ix+1,iy+1,iz] != $p2 && $ϕ[ix+1,iy+1,iz+1] != $p1)); $expr end ))
 end
+
+macro corner_xy(ϕ, p1, p2, expr)
+    bnd_chk = :(ix <= size($ϕ,1)-1 && iy <= size($ϕ,2)-1 && iz <= size($ϕ,3)-2)
+    cell_chk = :( if $ϕ[ix  ,iy  ,iz+1] == $p1 np1 += 1 elseif $ϕ[ix  ,iy  ,iz+1] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy  ,iz+1] == $p1 np1 += 1 elseif $ϕ[ix+1,iy  ,iz+1] == $p2 np2 += 1 end;
+                  if $ϕ[ix  ,iy+1,iz+1] == $p1 np1 += 1 elseif $ϕ[ix  ,iy+1,iz+1] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy+1,iz+1] == $p1 np1 += 1 elseif $ϕ[ix+1,iy+1,iz+1] == $p2 np2 += 1 end; )
+    esc(:( if $bnd_chk; np1 = 0; np2 = 0; $cell_chk; if np1 < 2 && np2 > 0; $expr; end; end ))
+end
+
+macro corner_xz(ϕ, p1, p2, expr)
+    bnd_chk = :(ix <= size($ϕ,1)-1 && iy <= size($ϕ,2)-2 && iz <= size($ϕ,3)-1)
+    cell_chk = :( if $ϕ[ix  ,iy+1,iz  ] == $p1 np1 += 1 elseif $ϕ[ix  ,iy+1,iz  ] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy+1,iz  ] == $p1 np1 += 1 elseif $ϕ[ix+1,iy+1,iz  ] == $p2 np2 += 1 end;
+                  if $ϕ[ix  ,iy+1,iz+1] == $p1 np1 += 1 elseif $ϕ[ix  ,iy+1,iz+1] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy+1,iz+1] == $p1 np1 += 1 elseif $ϕ[ix+1,iy+1,iz+1] == $p2 np2 += 1 end; )
+    esc(:( if $bnd_chk; np1 = 0; np2 = 0; $cell_chk; if np1 < 2 && np2 > 0; $expr; end; end ))
+end
+
+macro corner_yz(ϕ, p1, p2, expr)
+    bnd_chk = :(ix <= size($ϕ,1)-2 && iy <= size($ϕ,2)-1 && iz <= size($ϕ,3)-1)
+    cell_chk = :( if $ϕ[ix+1,iy  ,iz  ] == $p1 np1 += 1 elseif $ϕ[ix+1,iy  ,iz  ] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy  ,iz+1] == $p1 np1 += 1 elseif $ϕ[ix+1,iy  ,iz+1] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy+1,iz  ] == $p1 np1 += 1 elseif $ϕ[ix+1,iy+1,iz  ] == $p2 np2 += 1 end;
+                  if $ϕ[ix+1,iy+1,iz+1] == $p1 np1 += 1 elseif $ϕ[ix+1,iy+1,iz+1] == $p2 np2 += 1 end; )
+    esc(:( if $bnd_chk; np1 = 0; np2 = 0; $cell_chk; if np1 < 2 && np2 > 0; $expr; end; end ))
+end
