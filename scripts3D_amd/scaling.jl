98653762764a452303b3b5e342582e5feb6aed21
Ludovic Raess <ludovic.rass@gmail.com>
2022-11-28 00:04:15 +0100

    Update plot

--- scripts3D_amd/scaling.jl
+++ scripts3D_amd/scaling.jl
@@ -9,11 +9,11 @@ nprocs = (1, 8, 16, 32, 64)
 T_eff  = (225.34, 223.05, 220.95, 219.35, 219.28)
 
 T_effn = T_eff./T_eff[1]
-nxyz   =  896^3
+nxyz   =  511^3
 
 # ENV["GKSwstype"]="nul"
 
 png(plot(collect(nprocs),collect(T_effn),
          xticks=(nprocs, string.(nprocs)), legend=false,
          xlabel="Number of AMD MI250x GPUs", ylabel="Parallel efficiency",
-         dpi=150,size=(600, 380)),"weak_scale_lumi2.png")
+         dpi=150,size=(600, 380)),"weak_scale_lumi.png")
