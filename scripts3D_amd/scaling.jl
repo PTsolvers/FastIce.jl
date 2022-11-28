38a8f9c7c8c18bfa300e9ddcf1fd23e86bbc51b7
Ludovic Raess <ludovic.rass@gmail.com>
2022-11-28 15:08:45 +0100

    Improve scaling

--- scripts3D_amd/scaling.jl
+++ scripts3D_amd/scaling.jl
@@ -4,9 +4,9 @@ default(fontfamily="Computer Modern", linewidth=4,  markershape=:circle, markers
         framestyle=:box, fillalpha=0.4, margin=5mm)
 scalefontsizes(); scalefontsizes(1.3)
 
-nprocs = (1, 8, 16, 32, 64)
+nprocs = (1, 8, 16, 32, 64, 128, 256)
 
-T_eff  = (225.34, 223.05, 220.95, 219.35, 219.28)
+T_eff  = (225.34, 223.05, 220.95, 219.35, 218.3, 217.4, 217.5)
 
 T_effn = T_eff./T_eff[1]
 nxyz   =  511^3
