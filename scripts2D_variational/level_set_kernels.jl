b37ad5a69e5fde171cb7b1eea1b0de3e5f79ef61
Ivan Utkin <iutkin@ethz.ch>
2023-04-12 11:31:45 +0200

    Remove comments

--- scripts2D_variational/level_set_kernels.jl
+++ scripts2D_variational/level_set_kernels.jl
@@ -1,10 +1,5 @@
 include("geometry.jl")
 
-# @tiny function _kernel_compute_levelset_from_polygon!(ψ, xv, yv, mc)
-#     ix, iy = @indices()
-#     @inbounds ψ[ix, iy] = signed_distance(Point(xv[ix], yv[iy]), mc)
-# end
-
 @tiny function _kernel_compute_levelset_from_polygon!(op, ψ, xv, yv, mc)
     ix, iy = @indices()
     @inbounds ψ[ix, iy] = op(ψ[ix, iy], signed_distance(Point(xv[ix], yv[iy]), mc))
