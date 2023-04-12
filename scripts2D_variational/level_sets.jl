47ab0b59c2d70b782c401d5b117ca3ef72ad7f0f
Ivan Utkin <iutkin@ethz.ch>
2023-04-12 13:33:47 +0200

    WIP support for field extension

--- scripts2D_variational/level_sets.jl
+++ scripts2D_variational/level_sets.jl
@@ -7,4 +7,12 @@ function compute_levelset!(op, Ψ, xv, yv, mc)
     return
 end
 
+# by default, compute union of new and current levelset
 compute_levelset!(Ψ, xv, yv, mc) = compute_levelset!(min, Ψ, xv, yv, mc)
+
+const _extrapolate_with_levelset! = _kernel_extrapolate_with_levelset!(get_device())
+
+function extrapolate_with_levelset!(∂A_∂τ,A,Ψ,Δx,Δy)
+    wait(_extrapolate_with_levelset!(∂A_∂τ,A,Ψ,Δx,Δy; ndrange=(axes(A,1)[2:end-1],axes(A,2)[2:end-1])))
+    return
+end
