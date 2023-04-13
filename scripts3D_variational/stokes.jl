6c7cdd3ad7c8424332dde58c7bcd7f7349a92eb4
Ivan Utkin <iutkin@ethz.ch>
2023-04-13 13:50:28 +0200

    Change scripts

--- scripts3D_variational/stokes.jl
+++ scripts3D_variational/stokes.jl
@@ -11,14 +11,14 @@ end
 function update_V!(V, Pr, τ, ηs, wt, nudτ, ρg, dx, dy, dz; bwidth)
     V_inn = (x = inn(V.x), y = inn(V.y), z = inn(V.z))
     ranges = split_ndrange(axes(Pr),bwidth)
-    ie,oe =  hide_comm(ranges) do ndrange
+    ie,oe  =  hide_comm(ranges) do ndrange
         _update_V!(V_inn, Pr, τ, ηs, wt, nudτ, ρg, dx, dy, dz; ndrange)
     end
     wait.(oe)
-    update_halo!(V.x,V.y,V.z)
     bc_x_neumann!(0.0,V.y,V.z)
     bc_y_neumann!(0.0,V.x,V.z)
     bc_z_neumann!(0.0,V.x,V.y)
+    update_halo!(V.x,V.y,V.z)
     wait(ie)
     return
 end
