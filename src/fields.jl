5fc986618d6373adee6e73028b009f5d205d6a73
Ivan Utkin <iutkin@ethz.ch>
2023-07-06 21:27:21 +0200

    WIP fields

--- src/fields.jl
+++ src/fields.jl
@@ -1,50 +1,29 @@
-export field_array, scalar_field, vector_field, tensor_field, volfrac_field
+module Fields
 
-@inline field_array(::Type{T}, args...) where {T} = TinyKernels.device_array(T, DEVICE, args...)
+export AbstractField
+export Field
 
-@inline scalar_field(::Type{T}, args...) where {T} = field_array(T, args...)
+abstract type AbstractField{T,N,L} end
 
-# 2D fields
-@inline vector_field(::Type{T}, nx, ny) where {T} = (
-    x=field_array(T, nx + 1, ny),
-    y=field_array(T, nx, ny + 1)
-)
+struct Field{T,N,L,D,H} <: AbstractField{T,N,L}
+    data::D
+    halo::H
+end
 
-@inline tensor_field(::Type{T}, nx, ny) where {T} = (
-    xx=field_array(T, nx, ny),
-    yy=field_array(T, nx, ny),
-    xy=field_array(T, nx - 1, ny - 1)
-)
+@inline data(f::Field) = f.data
+@inline halo(f::Field) = f.halo
 
-@inline volfrac_field(::Type{T}, nx, ny) where {T} = (
-    c=field_array(T, nx, ny),
-    x=field_array(T, nx + 1, ny),
-    y=field_array(T, nx, ny + 1),
-    xy=field_array(T, nx - 1, ny - 1)
-)
+import Base.@pure
 
-# 3D fields
-@inline vector_field(::Type{T}, nx, ny, nz) where {T} = (
-    x=field_array(T, nx + 1, ny, nz),
-    y=field_array(T, nx, ny + 1, nz),
-    z=field_array(T, nx, ny, nz + 1)
-)
+@pure Base.eltype(::Field{T}) where T = T
 
-@inline tensor_field(::Type{T}, nx, ny, nz) where {T} = (
-    xx=field_array(T, nx, ny, nz),
-    yy=field_array(T, nx, ny, nz),
-    zz=field_array(T, nx, ny, nz),
-    xy=field_array(T, nx - 1, ny - 1, nz - 2),
-    xz=field_array(T, nx - 1, ny - 2, nz - 1),
-    yz=field_array(T, nx - 2, ny - 1, nz - 1)
-)
+@inline Base.size(f::Field) = size(data(f))
 
-@inline volfrac_field(::Type{T}, nx, ny, nz) where {T} = (
-    c=field_array(T, nx, ny, nz),
-    x=field_array(T, nx + 1, ny, nz),
-    y=field_array(T, nx, ny + 1, nz),
-    z=field_array(T, nx, ny, nz + 1),
-    xy=field_array(T, nx - 1, ny - 1, nz - 2),
-    xz=field_array(T, nx - 1, ny - 2, nz - 1),
-    yz=field_array(T, nx - 2, ny - 1, nz - 1)
-)
+@inline interior_indices(f::Field{T,N,L,D,Nothing}) where {T,N,L,D} = axes(data(f))
+@inline interior_indices(f::Field) = UnitRange.(halo(f).+1,size(f).-halo(f))
+
+@inline interior(f::Field{T,N,L,D,Nothing}) where {T,N,L,D} = data(f)
+
+@inline interior(f::Field) = view(data(f), interior_indices(f)...)
+
+end
