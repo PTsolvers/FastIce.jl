f35fc304e2c6908a80524ea9fb0c34a820de28ad
Ivan Utkin <iutkin@ethz.ch>
2023-07-10 14:45:11 +0200

    WIP fields

--- src/fields.jl
+++ src/fields.jl
@@ -3,21 +3,31 @@ module Fields
 export AbstractField
 export Field
 
-abstract type AbstractField{T,N,L} end
+abstract type AbstractField{T,N,L} <: AbstractArray{T,N} end
+
+import Base.@pure
+
+@pure location(::AbstractField{T,N,L}) where {T,N,L} = L
+@pure location_instance(::AbstractField{T,N,L}) where {T,N,L} = L.instance
+@pure Base.eltype(::AbstractField{T}) where T = T
+@pure Base.ndims(::AbstractField{T,N}) where {T,N} = N
+
+Base.IndexStyle(::AbstractField) = IndexCartesian()
 
 struct Field{T,N,L,D,H} <: AbstractField{T,N,L}
     data::D
     halo::H
 end
 
+Field{L}(data::D, halo::H) where {L,D,H} = Field{D, ndims(data), L, D, H}(data, halo)
+
 @inline data(f::Field) = f.data
 @inline halo(f::Field) = f.halo
 
-import Base.@pure
-
-@pure Base.eltype(::Field{T}) where T = T
-
 @inline Base.size(f::Field) = size(data(f))
+@inline Base.parent(f::Field) = data(f)
+
+Base.@propagate_inbounds Base.getindex(f::Field, inds...) = getindex(data(f), inds...)
 
 @inline interior_indices(f::Field{T,N,L,D,Nothing}) where {T,N,L,D} = axes(data(f))
 @inline interior_indices(f::Field) = UnitRange.(halo(f).+1,size(f).-halo(f))
