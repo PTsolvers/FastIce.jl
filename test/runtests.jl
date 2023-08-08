f42e1bac81e9cfed4bb7c269bacc26fa8be5f9ac
Ivan Utkin <iutkin@ethz.ch>
2023-08-08 13:20:20 +0200

    Update tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,29 @@
 using Test
 
-using CUDA
-
 using FastIce.Grids
 using FastIce.BoundaryConditions
 using KernelAbstractions
 
-@testset "boundary conditions" begin
-    backend = CUDABackend()
-    grid    = CartesianGrid(origin = (0.0, 0.0, 0.0), extent = (1.0, 1.0, 1.0), size = (2, 2, 2))
-    field   = KernelAbstractions.zeros(backend, Float64, size(grid) .+ 2)
-    west_bc = DirichletBC{HalfCell}(1.0)
-    east_bc = DirichletBC{FullCell}(0.5)
+# add KA backends
+backends = KernelAbstractions.Backend[CPU(), ]
 
-    discrete_bcs_x!(backend, 256, size(grid))(grid, (field, ), (west_bc, ), (east_bc, ))
-    KernelAbstractions.synchronize(backend)
+using CUDA
+CUDA.functional() && push!(backends, CUDABackend())
 
-    @test all((field[1, 2:end-1, 2:end-1] .+ field[2, 2:end-1, 2:end-1]) ./ 2 .≈ west_bc.val) 
-    @test all(field[end, 2:end-1, 2:end-1] .≈ east_bc.val) 
+using AMDGPU
+AMDGPU.functional() && push!(backends, ROCBackend())
+
+@testset "backend $backend" for backend in backends
+    @testset "boundary conditions" begin
+        grid    = CartesianGrid(origin = (0.0, 0.0, 0.0), extent = (1.0, 1.0, 1.0), size = (2, 2, 2))
+        field   = KernelAbstractions.zeros(backend, Float64, size(grid) .+ 2)
+        west_bc = DirichletBC{HalfCell}(1.0)
+        east_bc = DirichletBC{FullCell}(0.5)
+
+        discrete_bcs_x!(backend, 256, size(grid))(grid, (field, ), (west_bc, ), (east_bc, ))
+        KernelAbstractions.synchronize(backend)
+
+        @test all((field[1, 2:end-1, 2:end-1] .+ field[2, 2:end-1, 2:end-1]) ./ 2 .≈ west_bc.val) 
+        @test all(field[end, 2:end-1, 2:end-1] .≈ east_bc.val) 
+    end
 end
