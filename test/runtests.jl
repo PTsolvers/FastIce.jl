ce55a65c45576875aa0349c44d3881ddb5a94b84
Ivan Utkin <iutkin@ethz.ch>
2023-08-08 13:12:43 +0200

    Add tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,21 @@
-# For unit and reference tests
+using Test
+
+using CUDA
+
+using FastIce.Grids
+using FastIce.BoundaryConditions
+using KernelAbstractions
+
+@testset "boundary conditions" begin
+    backend = CUDABackend()
+    grid    = CartesianGrid(origin = (0.0, 0.0, 0.0), extent = (1.0, 1.0, 1.0), size = (2, 2, 2))
+    field   = KernelAbstractions.zeros(backend, Float64, size(grid) .+ 2)
+    west_bc = DirichletBC{HalfCell}(1.0)
+    east_bc = DirichletBC{FullCell}(0.5)
+
+    discrete_bcs_x!(backend, 256, size(grid))(grid, (field, ), (west_bc, ), (east_bc, ))
+    KernelAbstractions.synchronize(backend)
+
+    @test all((field[1, 2:end-1, 2:end-1] .+ field[2, 2:end-1, 2:end-1]) ./ 2 .≈ west_bc.val) 
+    @test all(field[end, 2:end-1, 2:end-1] .≈ east_bc.val) 
+end
