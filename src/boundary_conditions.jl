1bdc954caee18eb9949af6511c47428dfaa8a3c9
Ivan Utkin <iutkin@ethz.ch>
2023-08-08 12:43:22 +0200

    WIP BCs

--- src/boundary_conditions.jl
+++ src/boundary_conditions.jl
@@ -1,142 +1,171 @@
 module BoundaryConditions
 
+export discrete_bcs_x!, DirichletBC, HalfCell, FullCell
+
+using FastIce.Grids
+
 using KernelAbstractions
 using Adapt
 
-@kernel function discrete_bcs_x!(f, grid, west_ix, east_ix, west_bc, east_bc)
+@kernel function discrete_bcs_x!(grid, fields, west_bcs, east_bcs)
     iy, iz = @index(Global, NTuple)
-    apply_west_bc!(f, grid, west_ix, iy + 1, iz + 1, west_bc)
-    apply_east_bc!(f, grid, east_ix, iy + 1, iz + 1, east_bc)
+    for ifield in eachindex(fields) 
+        apply_west_bc!(grid, fields[ifield], iy + 1, iz + 1, west_bcs[ifield])
+        apply_east_bc!(grid, fields[ifield], iy + 1, iz + 1, east_bcs[ifield])
+    end
 end
 
-@kernel function discrete_bcs_y!(f, grid, south_iy, north_iy, south_bc, north_bc)
-    ix, iz = @index(Global, NTuple)
-    apply_south_bc!(f, grid, ix + 1, south_iy, iz + 1, south_bc)
-    apply_north_bc!(f, grid, ix + 1, north_iy, iz + 1, north_bc)
-end
+struct HalfCell end
+struct FullCell end
 
-@kernel function discrete_bcs_z!(f, bot_iz, top_iz, bot_bc, top_bc)
-    ix, iy = @index(Global, NTuple)
-    apply_bot_bc!(f, grid, ix + 1, iy + 1, bot_iz, bot_bc)
-    apply_top_bc!(f, grid, ix + 1, iy + 1, top_iz, top_bc)
-end
-
-struct NoBC end
-
-@inline apply_west_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-@inline apply_east_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-
-@inline apply_south_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-@inline apply_north_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-
-@inline apply_bot_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-@inline apply_top_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
-
-struct Shift end
-struct NoShift end
-
-struct DirichletBC{T,HasShift}
+struct DirichletBC{FluxReconstruction, T}
     val::T
 end
 
-DirichletBC{HasShift}(val::T) where {T,HasShift} = DirichletBC{T,HasShift}(val)
+DirichletBC{FluxReconstruction}(val::T) where {FluxReconstruction, T} = DirichletBC{FluxReconstruction, T}(val)
+(bc::DirichletBC{FR, <:Number})(grid, i, j) where FR = bc.val
+(bc::DirichletBC{FR, <:AbstractArray})(grid, i, j) where FR = bc.val[i, j]
 
-(bc::DirichletBC{<:Number})(grid, i, j) = bc.val
-(bc::DirichletBC{<:AbstractArray})(grid, i, j) = @inbounds bc.val[i, j]
 
-Adapt.adapt_structure(to, f::DirichletBC) = Adapt.adapt(to, f.val)
+@inline get_flux(Δ, f2, bc::DirichletBC{HalfCell}, grid, i, j) = (bc(grid, i, j) - f2)/(0.5Δ)
+@inline get_flux(Δ, f2, bc::DirichletBC{FullCell}, grid, i, j) = (bc(grid, i, j) - f2)/Δ
 
-struct NeumannBC{T}
-    val::T
+@inline function apply_west_bc!(grid, f, iy, iz, bc::DirichletBC)
+    f[1, iy, iz] = f[2, iy, iz] + Δ(grid, 1) * get_flux(Δ(grid, 1), f[2, iy, iz], bc, grid, iy, iz)
 end
 
-(bc::NeumannBC{<:Number})(grid, i, j) = bc.val
-(bc::NeumannBC{<:AbstractArray})(grid, i, j) = @inbounds bc.val[i, j]
-
-Adapt.adapt_structure(to, f::NeumannBC) = Adapt.adapt(to, f.val)
-
-@inline bc1!(f, I, v) = @inbounds f[I] = v
-@inline bc2!(f, I1, I2, a, b) = @inbounds f[I1] = a + b * f[I2]
-
-@inline apply_west_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, iy, iz))
-@inline apply_east_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, iy, iz))
-
-@inline apply_south_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iz))
-@inline apply_north_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iz))
-
-@inline apply_bot_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iy))
-@inline apply_top_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iy))
-
-@inline function apply_west_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix + 1, iy, iz)
-    bc2!(f, I1, I2, 2.0 * bc(grid, iy, iz), -1.0)
-    return
+@inline function apply_east_bc!(grid, f, iy, iz, bc::DirichletBC)
+    f[end, iy, iz] = f[end-1, iy, iz] + Δ(grid, 1) * get_flux(Δ(grid, 1), f[end-1, iy, iz], bc, grid, iy, iz)
 end
 
-@inline function apply_east_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix - 1, iy, iz)
-    bc2!(f, I1, I2, 2.0 * bc(grid, iy, iz), -1.0)
-    return
-end
+# @kernel function discrete_bcs_y!(f, grid, south_iy, north_iy, south_bc, north_bc)
+#     ix, iz = @index(Global, NTuple)
+#     apply_south_bc!(f, grid, ix + 1, south_iy, iz + 1, south_bc)
+#     apply_north_bc!(f, grid, ix + 1, north_iy, iz + 1, north_bc)
+# end
 
-@inline function apply_south_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy + 1, iz)
-    bc2!(f, I1, I2, 2.0 * bc(grid, ix, iz), -1.0)
-    return
-end
+# @kernel function discrete_bcs_z!(f, bot_iz, top_iz, bot_bc, top_bc)
+#     ix, iy = @index(Global, NTuple)
+#     apply_bot_bc!(f, grid, ix + 1, iy + 1, bot_iz, bot_bc)
+#     apply_top_bc!(f, grid, ix + 1, iy + 1, top_iz, top_bc)
+# end
 
-@inline function apply_north_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy - 1, iz)
-    bc2!(f, I1, I2, 2.0 * bc(grid, ix, iz), -1.0)
-    return
-end
+# struct NoBC end
 
-@inline function apply_bot_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz + 1)
-    bc2!(f, I1, I2, 2.0 * bc(grid, ix, iy), -1.0)
-    return
-end
+# @inline apply_west_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
+# @inline apply_east_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
 
-@inline function apply_top_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz - 1)
-    bc2!(f, I1, I2, 2.0 * bc(grid, ix, iy), -1.0)
-    return
-end
+# @inline apply_south_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
+# @inline apply_north_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
 
-@inline function apply_west_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix + 1, iy, iz)
-    bc2!(f, I1, I2, -Δ(grid, 1) * bc(grid, iy, iz), 1.0)
-    return
-end
+# @inline apply_bot_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
+# @inline apply_top_bc!(f, grid, ix, iy, iz, ::NoBC) = nothing
 
-@inline function apply_east_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix - 1, iy, iz)
-    bc2!(f, I1, I2, Δ(grid, 1) * bc(grid, iy, iz), 1.0)
-    return
-end
+# struct Shift end
+# struct NoShift end
 
-@inline function apply_south_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy + 1, iz)
-    bc2!(f, I1, I2, -Δ(grid, 2) * bc(grid, ix, iz), 1.0)
-    return
-end
+# struct DirichletBC{T,HasShift}
+#     val::T
+# end
 
-@inline function apply_north_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy - 1, iz)
-    bc2!(f, I1, I2, Δ(grid, 2) * bc(grid, ix, iz), 1.0)
-    return
-end
+# DirichletBC{HasShift}(val::T) where {T,HasShift} = DirichletBC{T,HasShift}(val)
 
-@inline function apply_bot_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz + 1)
-    bc2!(f, I1, I2, -Δ(grid, 3) * bc(grid, ix, iy), 1.0)
-    return
-end
+# (bc::DirichletBC{<:Number})(grid, i, j) = bc.val
+# (bc::DirichletBC{<:AbstractArray})(grid, i, j) = @inbounds bc.val[i, j]
 
-@inline function apply_top_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
-    I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz - 1)
-    bc2!(f, I1, I2, Δ(grid, 3) * bc(grid, ix, iy), 1.0)
-    return
-end
+# Adapt.adapt_structure(to, f::DirichletBC) = Adapt.adapt(to, f.val)
+
+# struct NeumannBC{T}
+#     val::T
+# end
+
+# (bc::NeumannBC{<:Number})(grid, i, j) = bc.val
+# (bc::NeumannBC{<:AbstractArray})(grid, i, j) = @inbounds bc.val[i, j]
+
+# Adapt.adapt_structure(to, f::NeumannBC) = Adapt.adapt(to, f.val)
+
+# @inline bc1!(f, I, v) = @inbounds f[I] = v
+# @inline bc2!(f, I1, I2, a, b) = @inbounds f[I1] = a + b * f[I2]
+
+# @inline apply_west_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, iy, iz))
+# @inline apply_east_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, iy, iz))
+
+# @inline apply_south_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iz))
+# @inline apply_north_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iz))
+
+# @inline apply_bot_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iy))
+# @inline apply_top_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,NoShift}) where {T} = bc1!(f, CartesianIndex(ix, iy, iz), bc(grid, ix, iy))
+
+# @inline function apply_west_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix + 1, iy, iz)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, iy, iz), -1.0)
+#     return
+# end
+
+# @inline function apply_east_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix - 1, iy, iz)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, iy, iz), -1.0)
+#     return
+# end
+
+# @inline function apply_south_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy + 1, iz)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, ix, iz), -1.0)
+#     return
+# end
+
+# @inline function apply_north_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy - 1, iz)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, ix, iz), -1.0)
+#     return
+# end
+
+# @inline function apply_bot_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz + 1)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, ix, iy), -1.0)
+#     return
+# end
+
+# @inline function apply_top_bc!(f, grid, ix, iy, iz, bc::DirichletBC{T,Shift}) where {T}
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz - 1)
+#     bc2!(f, I1, I2, 2.0 * bc(grid, ix, iy), -1.0)
+#     return
+# end
+
+# @inline function apply_west_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix + 1, iy, iz)
+#     bc2!(f, I1, I2, -Δ(grid, 1) * bc(grid, iy, iz), 1.0)
+#     return
+# end
+
+# @inline function apply_east_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix - 1, iy, iz)
+#     bc2!(f, I1, I2, Δ(grid, 1) * bc(grid, iy, iz), 1.0)
+#     return
+# end
+
+# @inline function apply_south_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy + 1, iz)
+#     bc2!(f, I1, I2, -Δ(grid, 2) * bc(grid, ix, iz), 1.0)
+#     return
+# end
+
+# @inline function apply_north_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy - 1, iz)
+#     bc2!(f, I1, I2, Δ(grid, 2) * bc(grid, ix, iz), 1.0)
+#     return
+# end
+
+# @inline function apply_bot_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz + 1)
+#     bc2!(f, I1, I2, -Δ(grid, 3) * bc(grid, ix, iy), 1.0)
+#     return
+# end
+
+# @inline function apply_top_bc!(f, grid, ix, iy, iz, bc::NeumannBC)
+#     I1, I2 = CartesianIndex(ix, iy, iz), CartesianIndex(ix, iy, iz - 1)
+#     bc2!(f, I1, I2, Δ(grid, 3) * bc(grid, ix, iy), 1.0)
+#     return
+# end
 
 end
--- src/models/full_stokes/isothermal/isothermal.jl
+++ src/models/full_stokes/isothermal/isothermal.jl
@@ -30,16 +30,192 @@ end
 fields(model::IsothermalFullStokesModel) = model.fields
 grid(model::IsothermalFullStokesModel) = model.grid
 
-function set_stress_bcs!(bcs, args...)
-    for (side, bc) in bcs
-        set_bc!(Val(side), bc, args...)
-    end
-    return
-end
+# In x direction:
+# Pr[1] = - Pr[2] + 2*px
+# τ.xx[1] = -τ.xx[2]
+# τ.yz[1] = py
+# τ.xz[1] = pz
+# struct PrescribedTractionBC{Tx,Ty,Tz}
+#     px::Tx
+#     py::Ty
+#     pz::Tz
+# end
 
-function set_velocity_bcs!(V, Pr, τ, Δ, bcs)
-    # TODO
-end
+# function extract_x_bcs!(bc::PrescribedTractionBC)
+#     (V.x, NoBC())
+#     (V.y, NoBC())
+#     (V.z, NoBC())
+
+#     (Pr, DirichletBC{Shift}(bc.px))
+
+#     (τ.xx, DirichletBC{NoShift}(0.0))
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+
+#     (τ.xy, DirichletBC{NoShift}(bc.py))
+#     (τ.xz, DirichletBC{NoShift}(bc.pz))
+#     (τ.yz, NoBC())
+# end
+
+# function extract_y_bcs!(bc::PrescribedTractionBC)
+#     (V.x, NoBC())
+#     (V.y, NoBC())
+#     (V.z, NoBC())
+
+#     (Pr, DirichletBC{Shift}(bc.py))
+
+#     (τ.xx, NoBC())
+#     (τ.yy, DirichletBC{NoShift}(0.0))
+#     (τ.zz, NoBC())
+
+#     (τ.xy, DirichletBC{NoShift}(bc.px))
+#     (τ.xz, NoBC())
+#     (τ.yz, DirichletBC{NoShift}(bc.pz))
+# end
+
+# function extract_z_bcs!(bc::PrescribedTractionBC)
+#     (V.x, NoBC())
+#     (V.y, NoBC())
+#     (V.z, NoBC())
+
+#     (Pr, DirichletBC{Shift}(bc.pz))
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, DirichletBC{NoShift}(0.0))
+
+#     (τ.xy, NoBC())
+#     (τ.xz, DirichletBC{NoShift}(bc.px))
+#     (τ.yz, DirichletBC{NoShift}(bc.py))
+# end
+
+# # In x direction:
+# # V.x[1] = vn
+# # τyz[1] = px
+# # τxz[1] = py
+# struct FreeSlipBC{Tn,Tx,Ty}
+#     vn::Tn
+#     px::Tx
+#     py::Ty
+# end
+
+# function extract_x_bcs!(bc::FreeSlipBC)
+#     (V.x, DirichletBC{NoShift}(bc.vn))
+#     (V.y, NoBC())
+#     (V.z, NoBC())
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+
+#     (τ.xy, DirichletBC{Shift}(bc.px))
+#     (τ.xz, DirichletBC{Shift}(bc.py))
+#     (τ.yz, NoBC)
+# end
+
+# function extract_y_bcs!(bc::FreeSlipBC)
+#     (V.x, NoBC())
+#     (V.y, DirichletBC{NoShift}(bc.vn))
+#     (V.z, NoBC())
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+
+#     (τ.xy, DirichletBC{NoShift}(bc.px))
+#     (τ.xz, NoBC())
+#     (τ.yz, DirichletBC{NoShift}(bc.py))
+# end
+
+# function extract_y_bcs!(bc::FreeSlipBC)
+#     (V.x, NoBC())
+#     (V.y, NoBC())
+#     (V.z, DirichletBC{NoShift}(bc.vn))
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+
+#     (τ.xy, NoBC())
+#     (τ.xz, DirichletBC{NoShift}(bc.px))
+#     (τ.yz, DirichletBC{NoShift}(bc.py))
+# end
+
+# # In x direction
+# # Vx[1] = vx
+# # Vy[1] = -Vy[2] + 2*vy
+# # Vx[1] = -Vz[2] + 2*vz
+# struct PrescribedVelocityBC{Tx,Ty,Tz}
+#     vx::Tx
+#     vy::Ty
+#     vz::Tz
+# end
+
+# function extract_x_bcs!(bc::PrescribedVelocityBC)
+#     (V.x, DirichletBC{NoShift}(bc.vx))
+#     (V.y, DirichletBC{Shift}(bc.vy))
+#     (V.z, DirichletBC{Shift}(bc.vz))
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+    
+#     (τ.xy, NoBC())
+#     (τ.xz, NoBC())
+#     (τ.yz, NoBC())
+# end
+
+# function extract_y_bcs!(bc::PrescribedVelocityBC)
+#     (V.x, DirichletBC{Shift}(bc.vx))
+#     (V.y, DirichletBC{NoShift}(bc.vy))
+#     (V.z, DirichletBC{Shift}(bc.vz))
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+    
+#     (τ.xy, NoBC())
+#     (τ.xz, NoBC())
+#     (τ.yz, NoBC())
+# end
+
+# function extract_z_bcs!(bc::PrescribedVelocityBC)
+#     (V.x, DirichletBC{Shift}(bc.vx))
+#     (V.y, DirichletBC{Shift}(bc.vy))
+#     (V.z, DirichletBC{NoShift}(bc.vz))
+
+#     (Pr, NoBC())
+
+#     (τ.xx, NoBC())
+#     (τ.yy, NoBC())
+#     (τ.zz, NoBC())
+    
+#     (τ.xy, NoBC())
+#     (τ.xz, NoBC())
+#     (τ.yz, NoBC())
+# end
+
+# function set_stress_bcs!(model::IsothermalFullStokesModel)
+#     nx, ny, nz = size(model.grid)
+#     discrete_bcs_x!(model.backend, 256, (ny, nz))(f, grid, west_ix, east_ix, west_bc, east_bc)
+#     discrete_bcs_y!(model.backend, 256, (nx, nz))(f, grid, west_ix, east_ix, west_bc, east_bc)
+#     discrete_bcs_z!(model.backend, 256, (nx, ny))(f, grid, west_ix, east_ix, west_bc, east_bc)
+#     return
+# end
+
+# function set_velocity_bcs!(model::IsothermalFullStokesModel)
+#     # TODO
+# end
 
 function advance_iteration!(model::IsothermalFullStokesModel, t, Δt; async = true)
     (; Pr, τ, V, η) = model.fields
