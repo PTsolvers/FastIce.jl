beecbf2c65fb4cd83f71ae185b67f3113569689d
Ivan Utkin <iutkin@ethz.ch>
2023-08-14 16:58:31 +0200

    Fix Adapt for fields

--- src/fields.jl
+++ src/fields.jl
@@ -79,7 +79,7 @@ halo(f::Field) = f.halo
 halo(f::Field, dim) = f.halo[dim]
 halo(f::Field, dim, side) = f.halo[dim][side]
 
-Adapt.adapt_structure(to, f::Field) = Adapt.adapt(to, f.data)
+Adapt.adapt_structure(to, f::Field) = Field(Adapt.adapt(to, f.data), f.halo, f.inidices)
 
 Base.@propagate_inbounds Base.getindex(f::Field, inds...) = getindex(f.data, inds...)
 Base.@propagate_inbounds Base.setindex!(f::Field, val, inds...) = setindex!(f.data, val, inds...)
