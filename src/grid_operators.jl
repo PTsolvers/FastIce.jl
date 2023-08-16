95180870d8d1f5569c7d9bdecde5a3b970055ed9
luraess <ludovic.rass@gmail.com>
2023-08-16 08:54:31 +0200

    Rename macros

--- src/grid_operators.jl
+++ src/grid_operators.jl
@@ -1,8 +1,8 @@
 module GridOperators
 
 export IDX, IDY, IDZ
-export ∂ᵛx, ∂ᵛy, ∂ᵛz, aᵛx, aᵛy, aᵛz, aᵛxy, aᵛxz, aᵛyz, mlᵛx, mlᵛy, mlᵛz
-export ∂ᶜx, ∂ᶜy, ∂ᶜz, aᶜx, aᶜy, aᶜz, aᶜxy, aᶜxz, aᶜyz, mlᶜx, mlᶜy, mlᶜz
+export ∂ᵛx, ∂ᵛy, ∂ᵛz, avᵛx, avᵛy, avᵛz, avᵛxy, avᵛxz, avᵛyz, maxlᵛx, maxlᵛy, maxlᵛz
+export ∂ᶜx, ∂ᶜy, ∂ᶜz, avᶜx, avᶜy, avᶜz, avᶜxy, avᶜxz, avᶜyz, maxlᶜx, maxlᶜy, maxlᶜz
 
 import Base.@propagate_inbounds
 
@@ -18,28 +18,28 @@ const IDZ = CartesianIndex(0, 0, 1)
 @propagate_inbounds ∂ᵛy(fc, I) = fc[I] - fc[I - IDY]
 @propagate_inbounds ∂ᵛz(fc, I) = fc[I] - fc[I - IDZ]
 
-@propagate_inbounds aᶜx(fv, I) = 0.5 * (fv[I] + fv[I + IDX])
-@propagate_inbounds aᶜy(fv, I) = 0.5 * (fv[I] + fv[I + IDY])
-@propagate_inbounds aᶜz(fv, I) = 0.5 * (fv[I] + fv[I + IDZ])
+@propagate_inbounds avᶜx(fv, I) = 0.5 * (fv[I] + fv[I + IDX])
+@propagate_inbounds avᶜy(fv, I) = 0.5 * (fv[I] + fv[I + IDY])
+@propagate_inbounds avᶜz(fv, I) = 0.5 * (fv[I] + fv[I + IDZ])
 
-@propagate_inbounds aᵛx(fc, I) = 0.5 * (fc[I] + fc[I - IDX])
-@propagate_inbounds aᵛy(fc, I) = 0.5 * (fc[I] + fc[I - IDY])
-@propagate_inbounds aᵛz(fc, I) = 0.5 * (fc[I] + fc[I - IDZ])
+@propagate_inbounds avᵛx(fc, I) = 0.5 * (fc[I] + fc[I - IDX])
+@propagate_inbounds avᵛy(fc, I) = 0.5 * (fc[I] + fc[I - IDY])
+@propagate_inbounds avᵛz(fc, I) = 0.5 * (fc[I] + fc[I - IDZ])
 
-@propagate_inbounds aᶜxy(fv, I) = 0.25 * (fv[I] + fv[I + IDX] + fv[I + IDY] + fv[I + IDX + IDY])
-@propagate_inbounds aᶜxz(fv, I) = 0.25 * (fv[I] + fv[I + IDX] + fv[I + IDZ] + fv[I + IDX + IDZ])
-@propagate_inbounds aᶜyz(fv, I) = 0.25 * (fv[I] + fv[I + IDY] + fv[I + IDZ] + fv[I + IDY + IDZ])
+@propagate_inbounds avᶜxy(fv, I) = 0.25 * (fv[I] + fv[I + IDX] + fv[I + IDY] + fv[I + IDX + IDY])
+@propagate_inbounds avᶜxz(fv, I) = 0.25 * (fv[I] + fv[I + IDX] + fv[I + IDZ] + fv[I + IDX + IDZ])
+@propagate_inbounds avᶜyz(fv, I) = 0.25 * (fv[I] + fv[I + IDY] + fv[I + IDZ] + fv[I + IDY + IDZ])
 
-@propagate_inbounds aᵛxy(fc, I) = 0.25 * (fc[I] + fc[I - IDX] + fc[I - IDY] + fc[I - IDX - IDY])
-@propagate_inbounds aᵛxz(fc, I) = 0.25 * (fc[I] + fc[I - IDX] + fc[I - IDZ] + fc[I - IDX - IDZ])
-@propagate_inbounds aᵛyz(fc, I) = 0.25 * (fc[I] + fc[I - IDY] + fc[I - IDZ] + fc[I - IDY - IDZ])
+@propagate_inbounds avᵛxy(fc, I) = 0.25 * (fc[I] + fc[I - IDX] + fc[I - IDY] + fc[I - IDX - IDY])
+@propagate_inbounds avᵛxz(fc, I) = 0.25 * (fc[I] + fc[I - IDX] + fc[I - IDZ] + fc[I - IDX - IDZ])
+@propagate_inbounds avᵛyz(fc, I) = 0.25 * (fc[I] + fc[I - IDY] + fc[I - IDZ] + fc[I - IDY - IDZ])
 
-@propagate_inbounds mlᶜx(fv, I) = max(fv[I], fv[I + IDX])
-@propagate_inbounds mlᶜy(fv, I) = max(fv[I], fv[I + IDY])
-@propagate_inbounds mlᶜz(fv, I) = max(fv[I], fv[I + IDZ])
+@propagate_inbounds maxlᶜx(fv, I) = max(fv[I], fv[I + IDX])
+@propagate_inbounds maxlᶜy(fv, I) = max(fv[I], fv[I + IDY])
+@propagate_inbounds maxlᶜz(fv, I) = max(fv[I], fv[I + IDZ])
 
-@propagate_inbounds mlᵛx(fc, I) = max(fc[I], fc[I - IDX])
-@propagate_inbounds mlᵛy(fc, I) = max(fc[I], fc[I - IDY])
-@propagate_inbounds mlᵛz(fc, I) = max(fc[I], fc[I - IDZ])
+@propagate_inbounds maxlᵛx(fc, I) = max(fc[I], fc[I - IDX])
+@propagate_inbounds maxlᵛy(fc, I) = max(fc[I], fc[I - IDY])
+@propagate_inbounds maxlᵛz(fc, I) = max(fc[I], fc[I - IDZ])
 
 end
