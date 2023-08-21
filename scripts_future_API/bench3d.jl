a61c9dfc088da1e477a9d9e2d7d9d4ddef19f8a0
Ludovic Raess <ludovic.rass@gmail.com>
2023-08-21 17:55:48 +0200

    Update

--- scripts_future_API/bench3d.jl
+++ scripts_future_API/bench3d.jl
@@ -1,46 +1,32 @@
 using KernelAbstractions
 using MPI
 using CUDA
+using CairoMakie
 
 include("mpi_utils.jl")
 include("mpi_utils2.jl")
 
-# @kernel function do_work!(A, me, offset)
-#     ix, iy, iz = @index(Global, NTuple)
-#     ix += offset[1] - 1
-#     iy += offset[2] - 1
-#     iz += offset[3] - 1
-#     for _ in 1:10
-#         # if (ix > 1 && ix < size(A, 1)) &&
-#         #    (iy > 1 && iy < size(A, 2)) &&
-#         #    (iz > 1 && iz < size(A, 3))
-#             A[ix, iy, iz] = me
-#         # end
-#     end
-# end
-
 @kernel function diffusion_kernel!(A_new, A, h, _dx, _dy, _dz, offset)
     ix, iy, iz = @index(Global, NTuple)
-    # ix += offset[1] - 1
-    # iy += offset[2] - 1
-    # iz += offset[3] - 1
-    # if ix ∈ axes(A_new, 1)[2:end-1] && iy ∈ axes(A_new, 2)[2:end-1] && iz ∈ axes(A_new, 3)[2:end-1]
-    if (1 < ix < size(A_new, 1)) && (1 < iy < size(A_new, 2)) && (1 < iz < size(A_new, 3))
-        #= @inbounds =# 
-        A_new[ix, iy, iz] = A[ix, iy, iz] + h * ((A[ix-1, iy  , iz  ] + A[ix+1, iy  , iz  ] - 2.0 * A[ix, iy, iz]) * _dx * _dx +
-                                                 (A[ix  , iy-1, iz  ] + A[ix  , iy+1, iz  ] - 2.0 * A[ix, iy, iz]) * _dy * _dy +
-                                                 (A[ix  , iy  , iz-1] + A[ix  , iy  , iz+1] - 2.0 * A[ix, iy, iz]) * _dz * _dz  )
+    ix += offset[1] - 1
+    iy += offset[2] - 1
+    iz += offset[3] - 1
+    if ix ∈ axes(A_new, 1)[2:end-1] && iy ∈ axes(A_new, 2)[2:end-1] && iz ∈ axes(A_new, 3)[2:end-1]
+    # if (1 < ix < size(A_new, 1)) && (1 < iy < size(A_new, 2)) && (1 < iz < size(A_new, 3))
+        @inbounds A_new[ix, iy, iz] = A[ix, iy, iz] + h * ((A[ix-1, iy  , iz  ] + A[ix+1, iy  , iz  ] - 2.0 * A[ix, iy, iz]) * _dx * _dx +
+                                                           (A[ix  , iy-1, iz  ] + A[ix  , iy+1, iz  ] - 2.0 * A[ix, iy, iz]) * _dy * _dy +
+                                                           (A[ix  , iy  , iz-1] + A[ix  , iy  , iz+1] - 2.0 * A[ix, iy, iz]) * _dz * _dz  )
     end
 end
 
-function main(backend = CPU(), T::DataType = Float64, dims = (0, 0, 0))
+function main(backend=CPU(), T::DataType=Float64, dims=(0, 0, 0))
     # physics
     l = 10.0
     # numerics
     nt = 10
-    nx, ny, nz = 16, 16, 16
+    nx, ny, nz = 8, 8, 8
     b_width = (2, 2, 2)
-    # dims, comm, me, neighbors, coords = init_distributed(dims; init_MPI=true)
+    dims, comm, me, neighbors, coords = init_distributed(dims; init_MPI=true)
     dx, dy, dz = l ./ (nx, ny, nz)
     _dx, _dy, _dz = 1.0 ./ (dx, dy, dz)
     h = min(dx, dy ,dz)^2 / 6.1
@@ -50,7 +36,7 @@ function main(backend = CPU(), T::DataType = Float64, dims = (0, 0, 0))
     z_g = (iz, dz) -> (coords[3] * (nz - 2) + (iz-1)) * dz + dz/2
     # Gaussian
     # A_ini = zeros(T, nx, ny, nz)
-    # A_ini .= [exp(-(x_g(ix, dx) - l/2)^2 - (y_g(iy, dy) - l/2)^2 - (z_g(iz, dz) - l/2)^2) for ix=1:size(A,1), iy=1:size(A,2), iz=1:size(A,3)]
+    # A_ini .= [exp(-(x_g(ix, dx) - l/2)^2 - (y_g(iy, dy) - l/2)^2 - (z_g(iz, dz) - l/2)^2) for ix=1:size(A_ini, 1), iy=1:size(A_ini, 2), iz=1:size(A_ini, 3)]
     A_ini = rand(T, nx, ny, nz)
 
     A     = KernelAbstractions.allocate(backend, T, nx, ny, nz)
@@ -59,46 +45,42 @@ function main(backend = CPU(), T::DataType = Float64, dims = (0, 0, 0))
     KernelAbstractions.synchronize(backend)
     A_new = copy(A)
 
-    @show typeof(A) typeof(A_new)
-
     ### to be hidden later
     ranges = split_ndrange(A, b_width)
 
-    # exchangers = ntuple(Val(length(neighbors))) do dim
-    #     ntuple(2) do side
-    #         Exchanger(backend) do
-    #             rank = neighbors[dim][side]
-    #             if rank != -1
-    #                 recv_buf = get_recv_view(Val(side), Val(dim), A)
-    #                 recv = MPI.Irecv!(recv_buf,comm;source=rank)
-    #             end
+    exchangers = ntuple(Val(length(neighbors))) do dim
+        ntuple(2) do side
+            Exchanger(backend) do
+                rank = neighbors[dim][side]
+                if rank != -1
+                    recv_buf = get_recv_view(Val(side), Val(dim), A)
+                    recv = MPI.Irecv!(recv_buf,comm;source=rank)
+                end
 
-    #             I = 2*(dim-1) + side
+                I = 2*(dim-1) + side
 
-    #             diffusion_kernel!(backend, 256)(A_new, A, h, dx, dy, dz, first(ranges[I]); ndrange=size(ranges[I]))
-    #             KernelAbstractions.synchronize(backend)
+                diffusion_kernel!(backend, 256)(A_new, A, h, _dx, _dy, _dz, first(ranges[I]); ndrange=size(ranges[I]))
+                KernelAbstractions.synchronize(backend)
 
-    #             if rank != -1
-    #                 send_buf = get_send_view(Val(side), Val(dim), A)
-    #                 send = MPI.Isend(send_buf,comm;dest=rank)
-    #                 cooperative_test!(recv)
-    #                 cooperative_test!(send)
-    #             end
-    #         end
-    #     end
-    # end
+                if rank != -1
+                    send_buf = get_send_view(Val(side), Val(dim), A)
+                    send = MPI.Isend(send_buf,comm;dest=rank)
+                    cooperative_test!(recv)
+                    cooperative_test!(send)
+                end
+            end
+        end
+    end
     ### to be hidden later
-
-    @show ranges[end]
 
     # actions
     for it = 1:nt
-    #    diffusion_kernel!(backend, 256)(A_new, A, h, dx, dy, dz, first(ranges[end]); ndrange=size(A))
        diffusion_kernel!(backend, 256)(A_new, A, h, _dx, _dy, _dz, first(ranges[end]); ndrange=size(ranges[end]))
-    #     for dim in reverse(eachindex(neighbors))
-    #         notify.(exchangers[dim])
-    #         wait.(exchangers[dim])
-    #     end
+    #    diffusion_kernel!(backend, 256)(A_new, A, h, _dx, _dy, _dz, #= first(ranges[end]) =#; ndrange=size(A))
+        for dim in reverse(eachindex(neighbors))
+            notify.(exchangers[dim])
+            wait.(exchangers[dim])
+        end
         KernelAbstractions.synchronize(backend)
         A, A_new = A_new, A
     end
@@ -106,7 +88,16 @@ function main(backend = CPU(), T::DataType = Float64, dims = (0, 0, 0))
     # for dim in eachindex(neighbors)
     #     setdone!.(exchangers[dim])
     # end
-    # finalize_distributed(; finalize_MPI=true)
+
+    # @info "visu"
+    # fig = Figure(resolution=(1000,1000), fontsize=32)
+    # ax  = Axis(fig[1,1][1,1]; aspect=DataAspect(), xlabel="x", ylabel="y")
+    # plt = heatmap!(ax, 1:nx, 1:ny, Array(A)[:, :, Int(ceil(nz/2))]; colormap=:turbo)
+    # Colorbar(fig[1,1][1,2], plt)
+    # # display(fig)
+    # save("out3.png", fig)
+
+    finalize_distributed(; finalize_MPI=true)
     return
 end
 
