f3ca45d3ff5afb785132657ad909351bd3f0be5d
Ludovic Raess <ludovic.rass@gmail.com>
2023-08-30 14:20:26 +0200

    Update conditional testing

--- test/common.jl
+++ test/common.jl
@@ -3,11 +3,36 @@ using FastIce
 
 using KernelAbstractions
 
+function parse_flags!(args, flag; default=nothing, typ=typeof(default))
+    for f in args
+        startswith(f, flag) || continue
+
+        if f != flag
+            val = split(f, '=')[2]
+            if !(typ ≡ nothing && typ <: AbstractString)
+                val = parse(typ, val)
+            end
+        else
+            val = default
+        end
+
+        filter!(x -> x != f, args)
+        return true, val
+    end
+    return false, default
+end
+
 # add KA backends
 backends = KernelAbstractions.Backend[CPU(), ]
 
-using CUDA
-CUDA.functional() && push!(backends, CUDABackend())
+_, backend_name = parse_flags!(ARGS, "--backend"; default="CPU", typ=String)
 
-using AMDGPU
-AMDGPU.functional() && push!(backends, ROCBackend())
+@static if backend_name == "AMDGPU"
+    Pkg.add("AMDGPU")
+    using AMDGPU
+    AMDGPU.functional() && push!(backends, ROCBackend())
+elseif backend_name == "CUDA"
+    Pkg.add("CUDA")
+    using CUDA
+    CUDA.functional() && push!(backends, CUDABackend())
+end
