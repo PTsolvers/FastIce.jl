8fb6c0f8a6d8afd7ad289ec77e54d488f3f97625
Ivan Utkin <iutkin@ethz.ch>
2023-10-05 20:17:20 +0200

    Fix test

--- scripts_future_API/test_pipeline.jl
+++ scripts_future_API/test_pipeline.jl
@@ -2,14 +2,14 @@ using FastIce.Utils
 
 function main()
     pipes = [Pipeline() for _ in 1:6]
-    for iter in 1:100
+    for iter in 1:10
         for ip in eachindex(pipes)
             put!(pipes[ip]) do
-                sleep(1/60)
+                sleep(0.001)
                 println("  inside pipeline #$(ip)!")
             end
         end
-        sleep(1/10)
+        sleep(0.01)
         println("outside, iter #$(iter)!")
         take!.(pipes)
         println()
