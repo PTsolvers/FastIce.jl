86ed3545671f8bc72c32061c74b7ee0a61f3cce8
Ivan Utkin <iutkin@ethz.ch>
2023-10-13 03:06:59 +0200

    Add tests for utils

---
+++ test/test_utils.jl
@@ -0,0 +1,42 @@
+include("common.jl")
+
+using FastIce.Utils
+
+@testset verbose = true "pipelines" begin
+    @testset "pre" begin
+        a = 0
+        pipe = Pipeline(; pre=() -> a += 1)
+        put!(()->nothing, pipe)
+        wait(pipe)
+        @test a == 1
+        close(pipe)
+    end
+
+    @testset "post" begin
+        a = 0
+        pipe = Pipeline(; post=() -> a += 2)
+        put!(pipe) do
+            a -= 1
+        end
+        wait(pipe)
+        close(pipe)
+        @test a == 1
+    end
+
+    @testset "do work" begin
+        a = 0
+        pipe = Pipeline()
+        put!(pipe) do
+            a += 1
+        end
+        wait(pipe)
+        close(pipe)
+        @test a == 1
+    end
+
+    @testset "not running" begin
+        pipe = Pipeline()
+        close(pipe)
+        @test_throws ErrorException wait(pipe)
+    end
+end
