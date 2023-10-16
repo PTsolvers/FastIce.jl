bcb1bb6bfcf164cab2ee72b3733062c3fa14bdaa
Ivan Utkin <iutkin@ethz.ch>
2023-10-16 23:31:13 +0200

    Remove empty lines

--- src/FastIce.jl
+++ src/FastIce.jl
@@ -5,22 +5,15 @@ module FastIce
 using KernelAbstractions
 
 include("Grids/Grids.jl")
-
 include("GridOperators.jl")
 include("Logging.jl")
 include("Fields.jl")
 include("Architectures.jl")
-
 include("Utils/Utils.jl")
-
-
-include("Physics.jl")
-
 include("BoundaryConditions/BoundaryConditions.jl")
 include("Models/models.jl")
-
 include("KernelLaunch.jl")
-
 include("Distributed/Distributed.jl")
+include("Physics.jl")
 
 end # module
