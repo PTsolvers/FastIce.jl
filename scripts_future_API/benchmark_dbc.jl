56a99474d93572cf81cb185fd86cbf346a8a9bcf
Ivan Utkin <iutkin@ethz.ch>
2023-11-01 11:37:18 +0100

    Update gather

--- scripts_future_API/benchmark_dbc.jl
+++ scripts_future_API/benchmark_dbc.jl
@@ -18,9 +18,9 @@ end
 
 MPI.Init()
 
-arch = Architecture(CPU(), (0, 0))
-grid = CartesianGrid(; origin=(0.0, 0.0), extent=(1.0, 1.0), size=(5, 5))
-field = Field(backend(arch), grid, (Center(), Center()); halo=1)
+arch = Architecture(CPU(), (2, 2, 2))
+grid = CartesianGrid(; origin=(0.0, 0.0, 0.0), extent=(1.0, 1.0, 1.0), size=(5, 7, 5))
+field = Field(backend(arch), grid, (Center(), Center(), Center()); halo=1)
 
 me = global_rank(details(arch))
 
@@ -28,17 +28,17 @@ fill!(parent(field), Inf)
 
 bc = BoundaryConditionsBatch((field,), (DirichletBC{FullCell}(-me-10),))
 
-boundary_conditions = override_boundary_conditions(arch, ((bc, bc), (bc, bc)); exchange=true)
+boundary_conditions = override_boundary_conditions(arch, ((bc, bc), (bc, bc), (bc, bc)); exchange=true)
 
-hide_boundaries = HideBoundaries{2}(arch)
+hide_boundaries = HideBoundaries{3}(arch)
 
-outer_width = (2, 2)
+outer_width = (2, 2, 2)
 
 launch!(arch, grid, fill_field! => (field, me); location=location(field), hide_boundaries, boundary_conditions, outer_width)
 
-sleep(me)
-@show coordinates(details(arch))
-display(parent(field))
+# sleep(0.25me)
+# @show coordinates(details(arch))
+# display(parent(field))
 
 field_g = if global_rank(details(arch)) == 0
     KernelAbstractions.allocate(Architectures.backend(arch), eltype(field), dimensions(details(arch)) .* size(field))
--- src/Distributed/gather.jl
+++ src/Distributed/gather.jl
@@ -1,25 +1,22 @@
-function gather!(dst::Union{AbstractArray,Nothing}, src::AbstractArray, comm::MPI.Comm; root=0)
+function gather!(dst::Union{AbstractArray{T,N},Nothing}, src::AbstractArray{T,N}, comm::MPI.Comm; root=0) where {T,N}
     dims, _, _ = MPI.Cart_get(comm)
     dims = Tuple(dims)
     if MPI.Comm_rank(comm) == root
         # make subtype for gather
-        offset  = Tuple(0 for _ in 1:ndims(src))
+        offset  = Tuple(0 for _ in 1:N)
         subtype = MPI.Types.create_subarray(size(dst), size(src), offset, MPI.Datatype(eltype(dst)))
         subtype = MPI.Types.create_resized(subtype, 0, size(src, 1) * Base.elsize(dst))
         MPI.Types.commit!(subtype)
         # make VBuffer for collective communication
-        counts = fill(Cint(1), dims)
-        displs = zeros(Cint, dims)
-        d = zero(Cint)
-        for j in 1:dims[2]
-            for i in 1:dims[1]
-                displs[i, j] = d
-                d += 1
-            end
-            d += (size(src, 2) - 1) * dims[1]
+        counts  = fill(Cint(1), reverse(dims)) # gather one subarray from each MPI rank
+        displs  = zeros(Cint, reverse(dims))   # reverse dims since MPI Cart comm is row-major
+        csizes  = cumprod(size(src)[2:end] .* dims[1:end-1])
+        strides = (1, csizes...)
+        for I in CartesianIndices(displs)
+            offset = reverse(Tuple(I - oneunit(I)))
+            displs[I] = sum(offset .* strides)
         end
-        # transpose displs as cartesian communicator is row-major
-        recvbuf = MPI.VBuffer(dst, vec(counts), vec(displs'), subtype)
+        recvbuf = MPI.VBuffer(dst, vec(counts), vec(displs), subtype)
         MPI.Gatherv!(src, recvbuf, comm; root)
     else
         MPI.Gatherv!(src, nothing, comm; root)
