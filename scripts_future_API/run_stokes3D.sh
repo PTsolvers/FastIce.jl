2351d3a70a3e4e89872f5f33a58b92c376ef3d51
Ivan Utkin <iutkin@ethz.ch>
2023-11-15 15:34:33 +0100

    Remove setup from a script

--- scripts_future_API/run_stokes3D.sh
+++ scripts_future_API/run_stokes3D.sh
@@ -4,8 +4,4 @@ module load LUMI/22.08
 module load partition/G
 module load rocm/5.3.3
 
-export MPICH_GPU_SUPPORT_ENABLED=1
-
-export LD_PRELOAD=${CRAY_MPICH_ROOTDIR}/gtl/lib/libmpi_gtl_hsa.so
-
 julia --project -O3 tm_stokes_mpi_wip.jl
