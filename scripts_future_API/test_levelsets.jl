f5a135184bfb252ee5509a2de24e80c5ec94319c
Ivan Utkin <iutkin@ethz.ch>
2023-12-14 13:52:24 +0100

    Update script

--- scripts_future_API/test_levelsets.jl
+++ scripts_future_API/test_levelsets.jl
@@ -2,14 +2,18 @@
 using FastIceTools
 using FileIO
 using JLD
-using CUDA
 using FastIce
 using FastIce.Grids
+using FastIce.Fields
 using FastIce.LevelSets
+using FastIce.Architectures
+
+using KernelAbstractions
+using CUDA
 
 vavilov_path = "../data/vavilov.jld"
 
-function load_dem_on_GPU(path::String)
+function load_dem_on_GPU(path::String, arch::Architecture)
     data = load(path)
     x = data["DataElevation"].x
     y = data["DataElevation"].y
@@ -17,19 +21,23 @@ function load_dem_on_GPU(path::String)
     z_surf = data["DataElevation"].z_surf
     z_bed = data["DataElevation"].z_bed
     domain = data["DataElevation"].domain
-    nx = length(x)
-    ny = length(y)
+    nx = length(x) - 1
+    ny = length(y) - 1
     nz = 100
-    z = LinRange(domain.zmin, domain.zmax, nz)
-    Ψ        = CUDA.zeros(nx, ny, nz)
+    z        = LinRange(domain.zmin, domain.zmax, nz)
     Ψ_grid   = CartesianGrid(origin=(0.0,0.0,0.0), extent=(1.0,1.0,1.0), size=(nx, ny, nz))
-    dem_bed  = CuArray(z_bed)
-    dem_surf = CuArray(z_surf)
+    Ψ        = Field(arch, Ψ_grid, Vertex())
     dem_grid = CartesianGrid(origin=(0.0,0.0), extent=(1.0,1.0), size=(nx, ny))
+    dem_bed  = Field(arch, dem_grid, Vertex())
+    dem_surf = Field(arch, dem_grid, Vertex())
+    set!(dem_bed, z_bed)
+    set!(dem_surf, z_surf)
     return Ψ, dem_surf, dem_grid, Ψ_grid
 end
 
-@time compute_level_set_from_dem!(load_dem_on_GPU(vavilov_path)...)
+arch = Architecture(CUDABackend())
+
+@time compute_level_set_from_dem!(load_dem_on_GPU(vavilov_path, arch)...)
 
 # TODO: visualization 
 # paraview: save ad hdf5 with metadata of XDMF file 
