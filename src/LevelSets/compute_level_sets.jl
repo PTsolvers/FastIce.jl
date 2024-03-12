e043df0e3f9692944f55b99cc57db7ec9f8853bd
Ludovic Raess <ludovic.rass@gmail.com>
2024-03-12 13:32:09 +0100

    Update

--- src/LevelSets/compute_level_sets.jl
+++ src/LevelSets/compute_level_sets.jl
@@ -1,9 +1,9 @@
 """
-    init_level_set!(Ψ::Field, dem::Field, dem_grid::UniformGrid, Ψ_grid::UniformGrid, cutoff, R)
+    init_level_set!(Ψ::Field, dem::AbstractField, dem_grid::UniformGrid, Ψ_grid::UniformGrid, cutoff, R)
 
 Initialize level sets.
 """
-@kernel inbounds = true function init_level_set!(Ψ::Field, dem::Field, dem_grid::UniformGrid, Ψ_grid::UniformGrid, cutoff, R, O)
+@kernel inbounds = true function init_level_set!(Ψ::Field, dem::AbstractField, dem_grid::UniformGrid, Ψ_grid::UniformGrid, cutoff, R, O)
     I = @index(Global, NTuple)
     I = I + O
     x, y, z = coord(Ψ_grid, location(Ψ), I...)
@@ -13,11 +13,11 @@ Initialize level sets.
 end
 
 """
-    compute_level_set_from_dem!(arch::Architecture, Ψ::Field, dem::Field, dem_grid2D::UniformGrid, grid::UniformGrid, R=LinearAlgebra.I)
+    compute_level_set_from_dem!(arch::Architecture, Ψ::Field, dem::AbstractField, dem_grid2D::UniformGrid, grid::UniformGrid, R=LinearAlgebra.I)
 
 Compute level sets from dem.
 """
-function compute_level_set_from_dem!(arch::Architecture, Ψ::Field, dem::Field, dem_grid2D::UniformGrid, grid::UniformGrid, R=LinearAlgebra.I; outer_width=(16, 8, 4))
+function compute_level_set_from_dem!(arch::Architecture, Ψ::Field, dem::AbstractField, dem_grid2D::UniformGrid, grid::UniformGrid, R=LinearAlgebra.I; outer_width=(16, 8, 4))
     cutoff = 4maximum(spacing(grid))
     launch = Launcher(arch, grid; outer_width=outer_width)
     launch(arch, grid, init_level_set! => (Ψ, dem, dem_grid2D, grid, cutoff, R); bc=batch(grid, Ψ => Neumann(); exchange=Ψ))
